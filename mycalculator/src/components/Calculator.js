c8f45c636d78993f0528ad4cf6a9107c3786d942
kongnyc <bbsyoow@gmail.com>
2020-02-17 23:52:20 -0500

    try hanleButtonClick

--- mycalculator/src/components/Calculator.js
+++ mycalculator/src/components/Calculator.js
@@ -9,31 +9,40 @@ class Calculator extends React.Component{
             waitingForNewValue: false
     };
 
+    handleButtonClick = (e) => {
+        this.setState((prevState,props)=>{
+            debugger
+            return {
+                // displayValue:0
+                // displayValue:e.target.innerText
+            }
+        })
+    }
+
     render(){
-        let {displayValue,previousValue,operation,waitingForNewValue}=this.state;
+        // let {displayValue,previousValue,operation,waitingForNewValue}=this.state;
         return(
            <div className="display">
-
             <div className="calculator">
             <div className="col-12 inputview">0</div>
             <button className="button col-3">AC</button>
             <button className="button col-3">%</button> 
             <button className="button col-3">±</button> 
             <button className="button col-3 orange">÷</button>
-            <button className="button col-3">7 </button> 
-            <button className="button col-3">8</button> 
-            <button className="button col-3">9</button> 
+            <button className="button col-3" onClick={this.handleButtonClick}>7 </button> 
+            <button className="button col-3" onClick={this.handleButtonClick}>8</button> 
+            <button className="button col-3" onClick={this.handleButtonClick}>9</button> 
             <button className="button col-3 orange">x</button>
-            <button className="button col-3">4</button> 
-            <button className="button col-3">5</button> 
-            <button className="button col-3">6</button> 
+            <button className="button col-3" onClick={this.handleButtonClick}>4</button> 
+            <button className="button col-3" onClick={this.handleButtonClick}>5</button> 
+            <button className="button col-3" onClick={this.handleButtonClick}>6</button> 
             <button className="button col-3 orange">-</button>
-            <button className="button col-3">1</button> 
-            <button className="button col-3">2</button> 
-            <button className="button col-3">3</button> 
+            <button className="button col-3" onClick={this.handleButtonClick}>1</button> 
+            <button className="button col-3" onClick={this.handleButtonClick}>2</button> 
+            <button className="button col-3" onClick={this.handleButtonClick}>3</button> 
             <button className="button col-3 orange">+</button>
-            <button className="button col-6">0</button> 
-            <button className="button col-3">.</button> 
+            <button className="button col-6" onClick={this.handleButtonClick}>0</button> 
+            <button className="button col-3" onClick={this.handleButtonClick}>.</button> 
             <button className="button col-3 orange">=</button>
 
             </div>
