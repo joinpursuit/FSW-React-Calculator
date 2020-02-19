342eedb91180150a37c833d3d9e31121aff8fb1b
Ohidurr <Ohidurrahman96@gmail.com>
2020-02-19 17:36:18 -0500

    create react app calculator, component folder, calc.js master.css

---
+++ calculator/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Calculator from './components/calc';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
