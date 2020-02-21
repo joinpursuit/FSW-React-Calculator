484134d8e7bf4d387ae714e47e7adca88207b40a
Kevin Wong <kevinwong@pursuit.org>
2020-02-21 18:16:33 -0500

    create react app calculator_lab with component & css folders

---
+++ calculator_lab/src/components/Calculator.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Calculator extends Component {
+  state = {};
+
+  render() {
+    return <div className={"calculator"}></div>;
+  }
+}
+
+export default Calculator;
