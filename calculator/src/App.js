bbf804b08e9114eaa69424e1d32b42ef987dc53b
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-22 12:29:28 -0500

    start button functionality

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -4,19 +4,24 @@ import Screen from './Components/Screen/Screen'
 import Button from './Components/Button/Button';
 
 const addToScreen = (number) => {
-  console.log(number);
+  if(screenText === 0) screenText = number;
+  else screenText += number;
 } // End of addToScreen() function
 
 const changeValue = () => {
   console.log("Hi");
 } // End of changeValue() function
 
+const resetScreen = () => {
+  screenText = 0;
+} // End of resetScreen();
+
 function App() {
   return (
     <div className="calculator">
-      <Screen className="screen"/>
+      <Screen className="screen" text={screenText}/>
       <Button className="center first" onclick={changeValue} text={"+/-"}/>
-      <Button className="right first" onclick={changeValue} text={"C"}/>
+      <Button className="right first" onclick={resetScreen} text={"C"}/>
       <Button className="left second" onclick={addToScreen} text={1}/>
       <Button className="center second" onclick={addToScreen} text={2}/>
       <Button className="right second" onclick={addToScreen} text={3}/>
--- calculator/src/Components/Screen/Screen.js
+++ calculator/src/Components/Screen/Screen.js
@@ -4,7 +4,7 @@ import './../../css/Screen.css'
 class Screen extends Component {
     render = () => {
         return (
-            <div className="screen">0</div>
+            <div className="screen">{this.props.text}</div>
         )
     }
 }
