7ccaadc1fc778a3c2cb7208c20da486bb93f2f35
rafidhossain1995 <rafidhossain@pursuit.org>
2020-02-22 15:15:33 -0500

    create buttons

---
+++ calculator-project/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Buttons from "./Component/buttons"
+
+function App() {
+  return (
+    <div className="App">
+    <Buttons/>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator-project/src/Component/buttons.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+
+class Buttons extends Component{
+
+
+
+
+render(){
+return(
+    <div className = "buttons">
+
+        <button name="0" className="0">0</button>
+        <button name="1" className="1">1</button>
+        <button name="2" className="2">2</button>
+        <button name="3" className="3">3</button>
+        <button name="4" className="4">4</button>
+        <button name="5" className="5">5</button>
+        <button name="6" className="6">6</button>
+        <button name="7" className="7">7</button>
+        <button name="8" className="8">8</button>
+        <button name="9" className="9">9</button>
+        <button name="add" className="add">+</button>
+        <button name="subtract" className="subtract">-</button>
+        <button name="multiply" className="multiply">x</button>
+        <button name="divide" className="divide">/</button>
+        <button name="equal" className="equal">=</button>
+        <button name="clear" className="clear">AC</button>
+        <button name="positive_negative" className="+-">+-</button>
+        
+
+
+
+
+
+
+
+    </div>
+)
+}
+}
+
+export default Buttons
