8fae95c960b86ac8b93df36992d09437c976e6a1
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-22 17:02:26 -0500

    add showComma functionality and decimal

--- calculator/src/Components/Calculator/Calculator.js
+++ calculator/src/Components/Calculator/Calculator.js
@@ -11,13 +11,28 @@ class Calculator extends Component {
     }
 
     addToScreen = (number) => {
+        this.setState({res: ""});
+        let {operandOne, operandTwo, operation} = this.state;
         if(number === "+" || number === "-" || number === "*" || number === "/") {
-            if(!this.state.operandOne) this.setState({operation: number, operandOne: "0"});
+            if(!operandOne) this.setState({operation: number, operandOne: "0"});
             else this.setState({operation: number});
-        } else if(!this.state.operation) {
-            this.setState({operandOne: this.state.operandOne + number});
+        } else if(!operation) {
+            if(number === ".") {
+                if(!this.state.operandOne.includes(".")) {
+                    this.setState({operandOne: operandOne + number});
+                }
+            } else {
+                this.setState({operandOne: operandOne + number});
+            }
+
         } else {
-            this.setState({operandTwo: this.state.operandTwo + number}) 
+            if(number === ".") {
+                if(!this.state.operandTwo.includes(".")) {
+                    this.setState({operandTwo: operandTwo + number});
+                }
+            } else {
+                this.setState({operandTwo: operandTwo + number});
+            }
         }
     } // End of addToScreen() function
 
@@ -39,23 +54,61 @@ class Calculator extends Component {
     calculate = () => {
         let {operation, operandOne, operandTwo} = this.state;
         if(!operation || !operandOne || !operandTwo) console.log("Missing values");
-        else if(operation === "+") this.add(operandOne, operandTwo);
-        else if(operation === "-") this.subtract(operandOne, operandTwo);
-        else if(operation === "*") this.multiply(operandOne, operandTwo);
-        else if(operation === "/") this.divide(operandOne, operandTwo);
+        else if(operation === "+") this.add(Number(operandOne), Number(operandTwo));
+        else if(operation === "-") this.subtract(Number(operandOne), Number(operandTwo));
+        else if(operation === "*") this.multiply(Number(operandOne), Number(operandTwo));
+        else if(operation === "/") this.divide(Number(operandOne), Number(operandTwo));
     } // End of calculate() function
 
     add = (num1, num2) => {
-        this.setState({operandOne: "", operandTwo: "", operation: null, res: num1 + num2});
+        this.setState({operandOne: num1 + num2, operandTwo: "", operation: null, res: num1 + num2});
     } // End of add() function
+
+    subtract = (num1, num2) => {
+        this.setState({operandOne: num1 - num2, operandTwo: "", operation: null, res: num1 - num2});
+    } // End of add() function
+
+    multiply = (num1, num2) => {
+        this.setState({operandOne: num1 * num2, operandTwo: "", operation: null, res: num1 * num2});
+    } // End of add() function
+
+    divide = (num1, num2) => {
+        this.setState({operandOne: num1 / num2, operandTwo: "", operation: null, res: num1 / num2});
+    } // End of add() function
+
+    placeCommas = (str) => {
+        let res = "";
+        let toSlice = "";
+        let decimal = "";
+
+        if(str.length > 3) {
+            for(let i = 0; i < str.length; i++) {
+                if(str[i] === ".") {
+                    toSlice = str.slice(0, i);
+                    decimal = str.slice(i);
+                }
+            } 
+        }
+
+        if(!toSlice) toSlice = str;
+
+        while(toSlice.length > 3) {
+            let sliced = toSlice.slice(-3);
+            res += "," + sliced;
+            toSlice = toSlice.slice(0, -3);
+        }
+
+        return toSlice + res + decimal;
+    } // End of placeCommas() function
     
     render = () => {
         let screenText = "0";
         let {operandOne, operation, operandTwo, res}  = this.state;
-        if(operandOne) screenText = operandOne;
-        if(operation) screenText = operandOne + operation;
-        if(operandTwo) screenText = operandOne + operation + operandTwo;
-        if(res) screenText = res;
+        if(operandOne) screenText = this.placeCommas(operandOne);
+        if(operation) screenText = this.placeCommas(operandOne) + operation;
+        if(operandTwo) screenText = this.placeCommas(operandOne) + operation + this.placeCommas(operandTwo);
+        if(res) screenText = this.placeCommas(res.toString());
+
         console.log(this.state);
         return (
             <div className={this.props.className}>
@@ -76,6 +129,7 @@ class Calculator extends Component {
                 <Button onclick={this.addToScreen} text={"9"}/>
                 <Button className="operand" onclick={this.addToScreen} text={"/"}/>
                 <Button className="zero" onclick={this.addToScreen} text={"0"}/>
+                <Button className="decimal" onclick={this.addToScreen} text={"."}/>
                 <Button className="operand" onclick={this.calculate} text={"="}/>
             </div>
         )
