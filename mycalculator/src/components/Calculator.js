53c72a7600ed622a2ade8eaabceea3682dee74a9
kongnyc <bbsyoow@gmail.com>
2020-02-23 10:17:54 -0500

    try new waiting new value after operation

--- mycalculator/src/components/Calculator.js
+++ mycalculator/src/components/Calculator.js
@@ -1,19 +1,21 @@
 import React from "react"
 
-// import "../css/calculator.css"
+import "../css/calculator.css"
 
 class Calculator extends React.Component{
     state={
             displayValue: 0,
             previousValue: null,
             operation: null,
-            waitingForNewValue: false
+            waitingForNewValue: false,
+            clearBtn:"AC"
     };
 
     handleNumberClick = (e) => {
         e.persist();
         let {value} = e.target
-        if(this.state.displayValue){
+        let{waitingForNewValue,displayValue}=this.state
+        if(displayValue){
             this.setState((prevState)=>{
                 return {displayValue:prevState.displayValue + value}
             })
@@ -21,6 +23,13 @@ class Calculator extends React.Component{
         else{
             this.setState({
                 displayValue: value
+            })
+        }
+    }
+    handleACClick = (e) => {
+        if(this.state.displayValue){
+            this.setState({
+                displayValue: 0
             })
         }
     }
@@ -40,62 +49,84 @@ class Calculator extends React.Component{
     }
 
     handleOperationClick = (e)=>{
-        let {previousValue,operation,waitingForNewValue}=this.state
-        debugger
-        
+        let {name} = e.target
+        let {displayValue}=this.state
+        // debugger
         this.setState({
-            // displayValue:-displayValue
+            waitingForNewValue:true,
+            operation:name,
+            previousValue:displayValue,
         })
+        console.log(displayValue)
     }
 
 
     render(){
         console.log(this.state);
-        let {displayValue}=this.state;
-        return(
-    
-    <div className="display">
-    <div className="calculator">
-    <div className="col-12 inputview">{displayValue}</div>
-    <button className="button col-3" name="AC">AC</button>
-    <button className="button col-3" name="%" onClick={(e)=>{
-    this.handlePercentageClick(e)}}>%</button> 
-    <button className="button col-3" name="±" onClick={(e)=>{
-    this.handleNegPotClick(e)}}>±</button> 
-    <button className="button col-3 orange" name="÷" onClick={(e)=>{
-    this.handleOperationClick(e)}}>÷</button>
-    <button className="button col-3"  name="displayValue" value="7" onClick={(e)=>{
-    this.handleNumberClick(e)}}>7</button> 
-    <button className="button col-3"  name="displayValue" value="8" onClick={(e)=>{
-    this.handleNumberClick(e)}}>8</button> 
-    <button className="button col-3"  name="displayValue" value="9" onClick={(e)=>{
-    this.handleNumberClick(e)}}>9</button> 
-    <button className="button col-3 orange" name="x" onClick={(e)=>{
-    this.handleOperationClick(e)}}>x</button>
-    <button className="button col-3"  name="displayValue" value="3" onClick={(e)=>{
-    this.handleNumberClick(e)}}>4</button> 
-    <button className="button col-3"  name="displayValue" value="5" onClick={(e)=>{
-    this.handleNumberClick(e)}}>5</button> 
-    <button className="button col-3"  name="displayValue" value="6" onClick={(e)=>{
-    this.handleNumberClick(e)}}>6</button> 
-    <button className="button col-3 orange" name="0" onClick={(e)=>{
-    this.handleOperationClick(e)}}>-</button>
-    <button className="button col-3"  name="displayValue" value="1" onClick={(e)=>{
-    this.handleNumberClick(e)}}>1</button> 
-    <button className="button col-3"  name="displayValue" value="2" onClick={(e)=>{
-    this.handleNumberClick(e)}}>2</button> 
-    <button className="button col-3"  name="displayValue" value="3" onClick={(e)=>{
-    this.handleNumberClick(e)}}>3</button> 
-    <button className="button col-3 orange"  name="+" onClick={(e)=>{
-    this.handleOperationClick(e)}}>+</button>
-    <button className="button col-6"  name="displayValue" value="0" onClick={(e)=>{
-    this.handleNumberClick(e)}}>0</button> 
-    <button className="button col-3"  name="displayValue" value="." onClick={(e)=>{
-    this.handleNumberClick(e)}}>.</button> 
-    <button className="button col-3 orange"  name="=" onClick={(e)=>{
-    this.handleOperationClick(e)}}>=</button>
-    </div>
-   </div>
+        let {displayValue,clearBtn}=this.state;
+        return( 
+                <div className="display">
+                <div className="calculator">
+                <div className="col-12 inputview">{displayValue}</div>
+                <button className="button col-3" name="AC" onClick={(e)=>{
+                this.handleACClick(e)}}>{clearBtn}</button>
+
+                <button className="button col-3" name="%" onClick={(e)=>{
+                this.handlePercentageClick(e)}}>%</button> 
+
+                <button className="button col-3" name="±" onClick={(e)=>{
+                this.handleNegPotClick(e)}}>±</button> 
+
+                <button className="button col-3 orange" name="÷" onClick={(e)=>{
+                this.handleOperationClick(e)}}>÷</button>
+
+                <button className="button col-3"  name="displayValue" value="7" onClick={(e)=>{
+                this.handleNumberClick(e)}}>7</button> 
+
+                <button className="button col-3"  name="displayValue" value="8" onClick={(e)=>{
+                this.handleNumberClick(e)}}>8</button> 
+
+                <button className="button col-3"  name="displayValue" value="9" onClick={(e)=>{
+                this.handleNumberClick(e)}}>9</button> 
+
+                <button className="button col-3 orange" name="x" onClick={(e)=>{
+                this.handleOperationClick(e)}}>x</button>
+
+                <button className="button col-3"  name="displayValue" value="3" onClick={(e)=>{
+                this.handleNumberClick(e)}}>4</button> 
+
+                <button className="button col-3"  name="displayValue" value="5" onClick={(e)=>{
+                this.handleNumberClick(e)}}>5</button> 
+
+                <button className="button col-3"  name="displayValue" value="6" onClick={(e)=>{
+                this.handleNumberClick(e)}}>6</button> 
+
+                <button className="button col-3 orange" name="-" onClick={(e)=>{
+                this.handleOperationClick(e)}}>-</button>
+
+                <button className="button col-3"  name="displayValue" value="1" onClick={(e)=>{
+                this.handleNumberClick(e)}}>1</button> 
+
+                <button className="button col-3"  name="displayValue" value="2" onClick={(e)=>{
+                this.handleNumberClick(e)}}>2</button> 
+
+                <button className="button col-3"  name="displayValue" value="3" onClick={(e)=>{
+                this.handleNumberClick(e)}}>3</button> 
+
+                <button className="button col-3 orange"  name="+" onClick={(e)=>{
+                this.handleOperationClick(e)}}>+</button>
+
+                <button className="button col-6"  name="displayValue" value="0" onClick={(e)=>{
+                this.handleNumberClick(e)}}>0</button> 
+
+                <button className="button col-3"  name="displayValue" value="." onClick={(e)=>{
+                this.handleNumberClick(e)}}>.</button> 
+
+                <button className="button col-3 orange"  name="=" onClick={(e)=>{
+                this.handleOperationClick(e)}}>=</button>
+
+                </div>
+            </div>
        
        )
     }
