a254bdb654c60e7134f2afd77490bbfc1b760d06
ashya <ashyamanning@pursuit.org>
2020-02-25 12:21:30 -0500

    testing

---
+++ calculator/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      You got this Queen!!
+    </div>
+  );
+}
+
+export default App;
