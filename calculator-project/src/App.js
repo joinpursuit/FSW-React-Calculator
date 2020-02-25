737f91a2b33b6b9e76accd137af76d40de15a8e4
Jovanni <jovanniluna@pursuit>
2020-02-25 11:56:39 -0500

    Added real clock

--- calculator-project/src/App.js
+++ calculator-project/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Form from "./components/form/form";
+import Clock from "react-live-clock";
 
 function App() {
   return (
     <div className="App">
+      <Clock className="clock" format={'HH:mm'} timezone={'US/Eastern'} />
+      <h1 className="title">Jovanni's Calculator Project</h1>
       <Form/>
     </div>
   );
