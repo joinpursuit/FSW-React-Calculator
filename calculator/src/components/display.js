c429095ef36acf765d48e655de4424cd89a0f4aa
Ohidurr <Ohidurrahman96@gmail.com>
2020-02-25 21:28:09 -0500

    add values to all numbers

--- calculator/src/components/display.js
+++ calculator/src/components/display.js
@@ -6,16 +6,16 @@ const Display = ({state, handleNumberClick}) => {
          <div className="display">
          <h2>{state.displayValue}</h2>
          <div className="AllNumbers">
-            <button className="0" value="0" onClick={(e) => handleNumberClick(e)}>0</button>
+            <button className="0" value="0" onClick={(e) =>handleNumberClick(e)}>0</button>
             <button className="1" value="1" onClick={(e) => handleNumberClick(e)}>1</button>
-            <button className="2">2</button>
-            <button className="3">3</button>
-            <button className="4">4</button>
-            <button className="5">5</button>
-            <button className="6">6</button>
-            <button className="7">7</button>
-            <button className="8">8</button>
-            <button className="9">9</button>
+            <button className="2" value="2" onClick={(e) => handleNumberClick(e)}>2</button>
+            <button className="3" value="3" onClick={(e) => handleNumberClick(e)}>3</button>
+            <button className="4" value="4" onClick={(e) => handleNumberClick(e)}>4</button>
+            <button className="5" value="5" onClick={(e) => handleNumberClick(e)}>5</button>
+            <button className="6" value="6" onClick={(e) => handleNumberClick(e)}>6</button>
+            <button className="7" value="7" onClick={(e) => handleNumberClick(e)}>7</button>
+            <button className="8" value="8" onClick={(e) => handleNumberClick(e)}>8</button>
+            <button className="9" value="9" onClick={(e) => handleNumberClick(e)}>9</button>
         </div>
          <div className="operators">
             <button className="+">+</button>
