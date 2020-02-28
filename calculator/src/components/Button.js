7ea73eb67d39d59e47b1de84b2fb12a40c386146
Jay Fowler <jcafowler@gmail.com>
2020-02-28 17:59:22 -0500

    Add all buttons to calculator

--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./Button.css"
 
 const isOperator = (value) => {
-  return !isNaN(value) || value === "." || value === "="
+  return !isNaN(value) || value === "." || value === " "
 }
 
 const Button = (props) => {
--- calculator/src/components/Calculator.js
+++ calculator/src/components/Calculator.js
@@ -7,15 +7,35 @@ class Calculator extends Component {
     return (
       <div className="wrapper">
         <div className="row">
+          <Button>AC</Button>
+          <Button>+/-</Button>
+          <Button>%</Button>
+          <Button>/</Button>
+        </div>
+        <div className="row">
           <Button>7</Button>
           <Button>8</Button>
           <Button>9</Button>
-          <Button>/</Button>
+          <Button>X</Button>
         </div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+        <div className="row">
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className="row">
+          <Button>0</Button>
+          <Button> </Button>
+          <Button>.</Button>
+          <Button>=</Button>
+        </div>
       </div>
     )
   }
