677a62e8022ac89700ba0d62283e864172e04fcf
Jay Fowler <jcafowler@gmail.com>
2020-02-28 18:19:56 -0500

    Change button styling

--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -1,13 +1,19 @@
 import React from "react"
 import "./Button.css"
 
-const isOperator = (value) => {
-  return !isNaN(value) || value === "." || value === " "
+const buttonClass = (value) => {
+  if (value === "AC" || value === "+/-" || value === "%" || value === ".") {
+    return "alternate"
+  } else if (isNaN(value)){
+    return "operator"
+  } else {
+    return null
+  }
 }
 
 const Button = (props) => {
   return (
-  <div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}>
+  <div className={`button-wrapper ${buttonClass(props.children)}`}>
     {props.children}
   </div>
   )
