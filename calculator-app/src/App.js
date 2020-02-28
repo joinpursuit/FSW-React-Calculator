b45454901edf3a62fee37c93efdb592119b12797
Phillip <phil.awich@gmail.com>
2020-02-28 15:22:08 -0500

    Display buttons

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -12,6 +12,24 @@ function App() {
           <Button>9</Button>
           <Button>/</Button>
         </div>
+        <div className="row">
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>X</Button>
+        </div>
+        <div className="row">
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className="row">
+          <Button>.</Button>
+          <Button>0</Button>
+          <Button>=</Button>
+          <Button>-</Button>
+        </div>
       </div>
     </div>
   );
