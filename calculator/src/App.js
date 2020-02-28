8bc83870a38803d93bd167b752ba87614af53488
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-02-27 22:19:02 -0500

    clean up bugs in App.js

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import Button from './components/Button';
+import Result from './components/Result';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <input type="number" disabled></input>
-      <Button />
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = { result: "" }
+  }
+
+  onClick = (symbol) => {
+    if(symbol === "="){
+      this.calculate()
+    } else if(symbol === "AC"){
+      this.reset()
+    } else {
+      this.setState({ result: this.state.result + symbol })
+    }
+  }
+
+  calculate = () => {
+    let resultCheck = ''
+    if(this.state.result.includes('--')){
+        resultCheck = this.state.result.replace('--','+')
+    }
+    else {
+        resultCheck = this.state.result
+    }
+    try {
+        this.setState({
+            // eslint-disable-next-line
+            result: (eval(resultCheck) || "" ) + ""
+        })
+    } catch (e) {
+        this.setState({
+            result: "error"
+        })
+    }
+};
+
+reset = () => {
+    this.setState({
+        result: ""
+    })
+};
+
+  render() {
+    return (
+      <div className="body">
+        <Result result={this.state.result}/>
+        <Button onClick={this.onClick}/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../CSS/Button.css';
 
 class Button extends React.Component {
     
@@ -6,29 +7,29 @@ class Button extends React.Component {
     render() {
         return(
             <div>
-                <button name="AC" onClick={e => this.props.onClick(e.target.name)}>AC</button>
-                <button name="neg" onClick={e => this.props.onClick(e.target.name)}>+/-</button>
-                <button name="percent" onClick={e => this.props.onClick(e.target.name)}>%</button>
-                <button name="divide" onClick={e => this.props.onClick(e.target.name)}>/</button>  <br/>
+                <button className="button" name="AC" onClick={e => this.props.onClick(e.target.name)}>AC</button>
+                <button className="button" name="-" onClick={e => this.props.onClick(e.target.name)}>+/-</button>
+                <button className="button" name="%" onClick={e => this.props.onClick(e.target.name)}>%</button>
+                <button className="button orange" name="/" onClick={e => this.props.onClick(e.target.name)}>/</button>  <br/>
                 
-                <button name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
-                <button name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
-                <button name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
-                <button name="multiply" onClick={e => this.props.onClick(e.target.name)}>x</button><br/>
+                <button className="button" name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
+                <button className="button" name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
+                <button className="button" name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
+                <button className="button orange" name="*" onClick={e => this.props.onClick(e.target.name)}>x</button><br/>
 
-                <button name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
-                <button name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
-                <button name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
-                <button name="subtract" onClick={e => this.props.onClick(e.target.name)}>-</button><br/>
+                <button className="button" name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
+                <button className="button" name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
+                <button className="button" name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
+                <button className="button orange" name="-" onClick={e => this.props.onClick(e.target.name)}>-</button><br/>
 
-                <button name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
-                <button name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
-                <button name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
-                <button name="addition" onClick={e => this.props.onClick(e.target.name)}>+</button><br/>
+                <button className="button" name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
+                <button className="button" name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
+                <button className="button" name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
+                <button className="button orange" name="+" onClick={e => this.props.onClick(e.target.name)}>+</button><br/>
                 
-                <button name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
-                <button name="decimal" onClick={e => this.props.onClick(e.target.name)}>.</button>
-                <button name="equal" onClick={e => this.props.onClick(e.target.name)}>=</button><br/>
+                <button className="button zero" name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
+                <button className="button" name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
+                <button className="button orange" name="=" onClick={e => this.props.onClick(e.target.name)}>=</button><br/>
             </div>
         )
     }
