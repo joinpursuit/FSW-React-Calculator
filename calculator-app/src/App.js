b9e4f0eb97ba470ebf9c65d511d0a9d47365b4ab
Phillip <phil.awich@gmail.com>
2020-02-28 15:34:32 -0500

    Create / Display input field

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,38 +1,50 @@
 import React from 'react';
 import './App.css';
-import Button from './components/Button'
+import Button from './components/Button';
+import Input from './components/Input'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="calc-wrapper">
-        <div className="row">
-          <Button>7</Button>
-          <Button>8</Button>
-          <Button>9</Button>
-          <Button>/</Button>
-        </div>
-        <div className="row">
-          <Button>4</Button>
-          <Button>5</Button>
-          <Button>6</Button>
-          <Button>X</Button>
-        </div>
-        <div className="row">
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-          <Button>+</Button>
-        </div>
-        <div className="row">
-          <Button>.</Button>
-          <Button>0</Button>
-          <Button>=</Button>
-          <Button>-</Button>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      input: ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="calc-wrapper">
+          <Input input={this.state.input}></Input>
+          <div className="row">
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>/</Button>
+          </div>
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>X</Button>
+          </div>
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>+</Button>
+          </div>
+          <div className="row">
+            <Button>.</Button>
+            <Button>0</Button>
+            <Button>=</Button>
+            <Button>-</Button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ calculator-app/src/components/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/Input.css';
+
+const Input = (props) => {
+    return(
+    <div className="input">
+        {props.input}
+    </div>
+    )
+}
+
+export default Input;
+
