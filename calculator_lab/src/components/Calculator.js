ad89bdb82ac179b837b8fab81d7d2c556fe032da
Kevin Wong <kevinwong@pursuit.org>
2020-02-28 12:14:23 -0500

    clean up buttonClicked method to use helper methods and make code look cleaner, fix bugs in certain edge cases

--- calculator_lab/src/components/Calculator.js
+++ calculator_lab/src/components/Calculator.js
@@ -14,34 +14,77 @@ class Calculator extends Component {
   };
 
   buttonClicked = buttonName => {
-    const { displayValue, previousValue, operation, isPrevious } = this.state;
+    const { displayValue } = this.state;
     if (buttonName === "=") {
-      if (
-        isPrevious === true &&
-        previousValue !== null &&
-        operation !== null &&
-        displayValue === ""
-      ) {
-        return;
-      } else {
-        this.setState(prevState => {
-          return {
-            displayValue: evaluate(
-              `${prevState.previousValue}${prevState.operation}${prevState.displayValue}`
-            ),
-            previousValue: "",
-            operation: "",
-            isPrevious: false
-          };
-        });
-      }
+      this.equals();
     } else if (
       buttonName === "+" ||
       buttonName === "-" ||
       buttonName === "*" ||
       buttonName === "/"
     ) {
+      this.operator(buttonName);
+    } else if (buttonName === "C") {
+      this.clear();
+    } else if (buttonName === "AC") {
+      this.allClear();
+    } else if (buttonName === "%") {
+      this.percent();
+    } else if (buttonName === "+/-") {
+      this.positiveNegative();
+    } else if (displayValue.length <= 9) {
+      this.setState(prevState => {
+        return { displayValue: prevState.displayValue + buttonName };
+      });
+    } else {
+      return;
+    }
+  };
+
+  equals = () => {
+    const { displayValue, previousValue, operation, isPrevious } = this.state;
+    if (
+      isPrevious === true &&
+      previousValue !== null &&
+      operation !== null &&
+      displayValue === ""
+    ) {
+      return;
+    } else {
+      this.setState(prevState => {
+        return {
+          displayValue: evaluate(
+            `${prevState.previousValue}${prevState.operation}${prevState.displayValue}`
+          ),
+          previousValue: "",
+          operation: "",
+          isPrevious: false
+        };
+      });
+    }
+  };
+
+  operator = buttonName => {
+    const { displayValue, previousValue, operation, isPrevious } = this.state;
+    if (
+      buttonName === "+" ||
+      buttonName === "-" ||
+      buttonName === "*" ||
+      buttonName === "/"
+    ) {
       if (
+        displayValue === "" &&
+        previousValue !== null &&
+        operation !== null &&
+        isPrevious === true
+      ) {
+        this.setState(prevState => {
+          return {
+            previousValue: prevState.previousValue,
+            operation: buttonName
+          };
+        });
+      } else if (
         displayValue !== null &&
         previousValue !== null &&
         operation !== null &&
@@ -53,7 +96,8 @@ class Calculator extends Component {
             previousValue: evaluate(
               `${prevState.previousValue}${prevState.operation}${prevState.displayValue}`
             ),
-            operation: buttonName
+            operation: buttonName,
+            isPrevious: true
           };
         });
       } else if (displayValue) {
@@ -65,54 +109,50 @@ class Calculator extends Component {
             isPrevious: true
           };
         });
-      } else if (
-        displayValue === "" &&
-        previousValue !== null &&
-        operation !== null &&
-        isPrevious === true
-      ) {
-        this.setState({ operation: buttonName });
       } else {
         return;
       }
-    } else if (buttonName === "C") {
-      if (
-        displayValue !== "" &&
-        previousValue !== null &&
-        operation !== null &&
-        isPrevious === true
-      ) {
-        this.setState({ displayValue: "" });
-      } else {
-        this.setState({
-          displayValue: "",
-          previousValue: "",
-          operation: "",
-          isPrevious: false
-        });
-      }
-    } else if (buttonName === "AC") {
+    }
+  };
+
+  clear = () => {
+    const { displayValue, previousValue, operation, isPrevious } = this.state;
+    if (
+      displayValue !== "" &&
+      previousValue !== null &&
+      operation !== null &&
+      isPrevious === true
+    ) {
+      this.setState({ displayValue: "" });
+    } else {
       this.setState({
         displayValue: "",
         previousValue: "",
         operation: "",
         isPrevious: false
       });
-    } else if (buttonName === "%") {
-      this.setState(prevState => {
-        return { displayValue: prevState.displayValue / 100 };
-      });
-    } else if (buttonName === "+/-") {
-      this.setState(prevState => {
-        return { displayValue: prevState.displayValue * -1 };
-      });
-    } else if (displayValue.length <= 9) {
-      this.setState(prevState => {
-        return { displayValue: prevState.displayValue + buttonName };
-      });
-    } else {
-      return;
     }
+  };
+
+  allClear = () => {
+    this.setState({
+      displayValue: "",
+      previousValue: "",
+      operation: "",
+      isPrevious: false
+    });
+  };
+
+  percent = () => {
+    this.setState(prevState => {
+      return { displayValue: prevState.displayValue / 100 };
+    });
+  };
+
+  positiveNegative = () => {
+    this.setState(prevState => {
+      return { displayValue: prevState.displayValue * -1 };
+    });
   };
 
   render() {
