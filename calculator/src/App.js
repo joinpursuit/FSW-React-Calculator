08103d12ffa5dd17f94099b221e416d71706d0ee
Syn Perez <perezsyn@gmail.com>
2020-02-28 19:02:18 -0500

    simple bases and component

---
+++ calculator/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calculator from './components/calculator'
+
+function App() {
+  return (
+    <div className="App">
+<h1>Calculator</h1>
+<Calculator/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator/src/components/calculator.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div>
+                <button>ac</button>
+                <button>+/-</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>0</button>
+                <button>+</button>
+                <button>-</button>
+                <button>x</button>
+                <button>÷</button>
+                <button>=</button>
+                <input type="text" placeholder="value"></input>
+            </div>
+        )
+    }
+}
