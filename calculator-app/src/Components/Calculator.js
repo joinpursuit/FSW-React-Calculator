113e5ff62a1282c75c07c7ca4fa7cf3e5dfee46d
DejaLFlynn <dejaflynn@pursuit.org>
2020-02-28 19:27:14 -0500

    working on button functionality

--- calculator-app/src/Components/Calculator.js
+++ calculator-app/src/Components/Calculator.js
@@ -6,9 +6,21 @@ class Calculator extends React.Component{
         display: 0
     }
     buttonValue(event){
-        let {value} = event.target
-        let {display} = this.state
+    
+      this.setState({display:event.target.value})
 
+    }
+    addition(event){
+        this.setState({result: (num1, num2) => num1 + num2})
+    }
+    subtraction(){
+        this.setState({result: (num1, num2) => num1 - num2})
+    }
+    multiplication(){
+        this.setState({result: (num1, num2) => num1 * num2})
+    }
+    division(){
+        this.setState({result: (num1, num2) => num1 / num2})
     }
 
     render(){
@@ -19,31 +31,31 @@ class Calculator extends React.Component{
 <div>
 
 <button className="Others">AC</button> 
-<button value = "7">7</button>
-<button value = "4 ">4</button>
-<button value = "1 ">1</button>
-<button className="Zero" >0</button>
+<button value = "7" onClick = {this.buttonValue}>7</button>
+<button value = "4 "onClick = {this.buttonValue} >4</button>
+<button value = "1 "onClick = {this.buttonValue}>1</button>
+<button className="Zero" value = "0 "onClick = {this.buttonValue}>0</button>
 </div>
 <div>
 
 <button className="Others">+/-</button>
-<button value = "8 ">8</button>
-<button value = "5 ">5</button>
-<button value = "3 ">3</button>
+<button value = "8 "onClick = {this.buttonValue}>8</button>
+<button value = "5 "onClick = {this.buttonValue}>5</button>
+<button value = "3 "onClick = {this.buttonValue}>3</button>
 </div>
 <div>
-<button className="Others">%</button>
-<button value = "9 ">9</button>
-<button value = "6">6</button>
-<button value = "2 ">2</button>
-<button >.</button>
+<button className="Others"value = "% "onClick = {this.buttonValue}>%</button>
+<button value = "9 "onClick = {this.buttonValue}>9</button>
+<button value = "6"onClick = {this.buttonValue}>6</button>
+<button value = "2 "onClick = {this.buttonValue}>2</button>
+<button value = ". "onClick = {this.buttonValue}>.</button>
 </div>
 <div>
 
-<button className="Operators">÷</button>
-<button className="Operators">x</button>
-<button className="Operators">-</button>
-<button className="Operators">+</button> 
+<button className="Operators"onClick = {this.division}value = "/">÷</button>
+<button className="Operators"onClick = {this.multiplication} value = "* ">x</button>
+<button className="Operators"onClick = {this.subtraction}value = " - ">-</button>
+<button className="Operators"onClick = {this.addition} value = " + ">+</button> 
     <div>
 <button className="Equal">=</button>
 
--- calculator-app/src/Components/Results.js
+++ calculator-app/src/Components/Results.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 class Results extends React.Component{
+    state={
+        num1: 0,
+        num2: 0,
+        equation: [],
+        result: 0
+    }
+
+    
+    
+    
+     
     render(){
+        const {equation}=this.state.equation 
+        const {result}=this.state.result 
         return(
             <div className="Results">
-        1 + 1 = 2
-
+      {equation}
+      {result}
+0123
         </div>
         )
     }
