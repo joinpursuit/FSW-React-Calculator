c47faf21ad6fa47f5664ad4e59b84a1a17d3bb03
BrandonBrown <brandonbrown@pursuit.org>
2020-02-29 13:29:12 -0500

    create conditions for operators

--- calculatorlab/src/Components/Button.js
+++ calculatorlab/src/Components/Button.js
@@ -7,7 +7,18 @@ class Button extends React.Component {
         prevValue: "",
         buttons: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "/", "*", "=", ".", "clear"]
     }
-    loadValue = (value) => {
+    loadValue = (value) => { //function to change state of displayValue/prevValue states
+        const { displayValue, prevValue } = this.state
+        const operators = ["+", "-", "/", "*", "=", ".", "clear"]
+        if(operators.includes(value)) {
+            if(value === "clear") { this.setState({displayValue: 0})} //clears display value and not prev value
+            if(value === "="){ this.setState({ displayValue: prevValue + displayValue }) } //does the equation for prev and current value
+            if(value === ".") { this.setState({displayValue: displayValue + value})} //adds decimal
+            
+            this.setState({prevValue: displayValue + value}) //adds operator to current value and sets it to prevValue state
+            this.setState({displayValue: 0}) //resets value back to 0 when and operator is clicked
+
+        }
         if(this.state.displayValue === 0){
             this.setState({displayValue: value})
         }else {
@@ -17,22 +28,21 @@ class Button extends React.Component {
     }
     handleClick = (e) => {
         e.preventDefault(); 
-        const { displayValue, prevValue } = this.state
+        const { displayValue } = this.state
         const operators = ["+", "-", "/", "*"]
         if(isNaN(e.target.value) ){  //condition for operators
             if(operators.includes(e.target.value)) {
                 console.log(e.target.value);
                 
-
             }
         }
-        if(displayValue === 0) {
+        if(displayValue === 0) { //condition to fix undefined error on joining inputs
             this.setState({displayValue: e.target.value})
         } else {
             this.setState({displayValue: displayValue + e.target.value})  
         }
      
-        return(
+        return( //returning the function that changes the state of my values
             this.loadValue(e.target.value) 
         ) 
       }
