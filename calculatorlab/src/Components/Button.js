3dbb444265a89410ca1fe10dd23356b6958ba368
BrandonBrown <brandonbrown@pursuit.org>
2020-02-29 11:04:27 -0500

    complete display func

--- calculatorlab/src/Components/Button.js
+++ calculatorlab/src/Components/Button.js
@@ -3,24 +3,49 @@ import DisplayValueForm from './DisplayValueForm'
 
 class Button extends React.Component {
     state = {
-        buttons: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "/", "*", "="],
-        display: ""
+        buttons: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "/", "*", "=", ".", "clear"],
+        displayValue: "",
+        prevValue: ""
     }
     loadValue = (value) => {
         this.setState({display:this.state.display + value})
-        console.log(this.state.display);
-        
+        console.log(this.state.display); 
     }
     handleClick = (e) => {
-        e.preventDefault();  
+        e.preventDefault(); 
+        const { displayValue, prevValue } = this.state
+        const operators = ["+", "-", "/", "*"]
+        if(isNaN(e.target.value) ){
+            if(operators.includes(e.target.value)) {
+
+            }
+        }
+        if(displayValue === "") {
+            this.setState({displayValue: e.target.value})
+        } else {
+            this.setState({displayValue: displayValue + e.target.value})  
+        }
+
              
         return(
             this.loadValue(e.target.value) 
         ) 
       }
-
+    operation = (operator) => {
+        const { displayValue, prevValue } = this.state
+        let equal = prevValue + operator + displayValue
+        this.setState({displayValue: equal})
+    }
+        
+    display = (value) => {
+        return (
+            <div>
+                <p>{value}</p> 
+            </div>
+        )
+    }
     render() {
-      const { buttons, display } = this.state
+      const { buttons, displayValue } = this.state
       let calcButtons = buttons.map(button => {
         //   debugger
         return( <button value={button} key={button} onClick={this.handleClick}>{button}</button> )
@@ -28,7 +53,7 @@ class Button extends React.Component {
       return (
         <div className= "buttonContainer">
           {calcButtons}
-          {/* <DisplayValueForm value={display}/>  */}
+          {/* <DisplayValueForm value={displayValue}/>  */}
         </div>
         
       );
