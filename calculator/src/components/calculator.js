dc9f83d130f0650ab971814341c722dc5a45598c
Syn Perez <perezsyn@gmail.com>
2020-02-29 23:05:50 -0500

    value added

--- calculator/src/components/calculator.js
+++ calculator/src/components/calculator.js
@@ -10,28 +10,38 @@ export default class Calculator extends Component {
         };
       }
 
+    inputChange = (e) => {
+        this.setState({input: this.state.input + e.target.value});
+        debugger
+      };
+
     render() {
         return (
             <div className = "buttonDiv">
+                <input type="text" value={this.state.input}></input>
+                <br/>
                 <button>ac</button>
                 <button>+/-</button>
-                <button>1</button>
+                <button>.</button>
+                <br/>
+                <button onClick={this.inputChange} value= {"1"}>1</button>
                 <button>2</button>
                 <button>3</button>
+                <br/>
                 <button>4</button>
                 <button>5</button>
                 <button>6</button>
+                <br/>
                 <button>7</button>
                 <button>8</button>
                 <button>9</button>
+                <br/>
                 <button>0</button>
-                <button>.</button>
                 <button>+</button>
                 <button>-</button>
                 <button>x</button>
                 <button>÷</button>
                 <button>=</button>
-                <input type="text" placeholder="value"></input>
             </div>
         )
     }
