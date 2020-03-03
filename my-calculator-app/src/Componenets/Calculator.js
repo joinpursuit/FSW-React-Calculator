d41a6b7354896431bcac622446b547d47177b7e9
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-03-02 21:25:45 -0500

    add buttons

--- my-calculator-app/src/Componenets/Calculator.js
+++ my-calculator-app/src/Componenets/Calculator.js
@@ -4,7 +4,22 @@ class Calculator extends Component {
   render () {
     return (
       <div>
-        Hello World
+        <button> 1 </button>
+        <button> 2 </button>
+        <button> 3 </button>
+        <button> 4 </button>
+        <button> 5 </button>
+        <button> 6 </button>
+        <button> 7 </button>
+        <button> 8 </button>
+        <button> 9 </button>
+        <button> * </button>
+        <button> - </button>
+        <button> + </button>
+        <button> ÷ </button>
+        <button> = </button>
+        <button> C </button>
+        <button> ± </button>
       </div>
     )
   }
