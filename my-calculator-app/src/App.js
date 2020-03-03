8782824eba3d045c5dfc048dfaeb45cf557c5768
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-03-02 20:18:23 -0500

    added button component

--- my-calculator-app/src/App.js
+++ my-calculator-app/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+    <Calculator/>
       
-       
     </div>
   );
 }
