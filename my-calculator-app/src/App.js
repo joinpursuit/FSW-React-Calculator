3e746a0c55b07b4a32e09cff291bd581aef5f882
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-03-06 01:07:49 -0500

    moved buttons and added handleClick

--- my-calculator-app/src/App.js
+++ my-calculator-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
+import Calculator from "./Componenets/Calculator"
 import './App.css';
 
 const App = () => {
--- my-calculator-app/src/Componenets/Calculator.js
+++ my-calculator-app/src/Componenets/Calculator.js
@@ -1,28 +1,47 @@
 import React, {Component} from "react";
 
+
+
 class Calculator extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showValue: "0",
+      previousValue: "",
+      
+    }
+  }
+  
+
   render () {
+    let {showValue} = this.state
     return (
-      <div>
-        <button> 1 </button>
-        <button> 2 </button>
-        <button> 3 </button>
-        <button> 4 </button>
-        <button> 5 </button>
-        <button> 6 </button>
-        <button> 7 </button>
-        <button> 8 </button>
-        <button> 9 </button>
-        <button> * </button>
-        <button> - </button>
-        <button> + </button>
-        <button> ÷ </button>
-        <button> = </button>
-        <button> C </button>
-        <button> ± </button>
+      <div className="clacBox">
+      <div> Display showValue={showValue}</div>
+        
+        <button handleClick={}> ÷ </button>
+        <button handleClick={}> % </button>
+        <button handleClick={}> ± </button>
+        <button handleClick={}> C </button>
+        <button handleClick={}> * </button>
+        <button handleClick={}> 9 </button>
+        <button handleClick={}> 8 </button>
+        <button handleClick={}> 7 </button>
+        <button handleClick={}> - </button>
+        <button handleClick={}> 6 </button>
+        <button handleClick={}> 5 </button>
+        <button handleClick={}> 4 </button>
+        <button handleClick={}> + </button>
+        <button handleClick={}> 3 </button>
+        <button handleClick={}> 2 </button>
+        <button handleClick={}> 1 </button>
+        <button handleClick={}> = </button>
+        <button handleClick={}> . </button>
+        <button handleClick={}> 0 </button>
       </div>
     )
   }
 }
 
 export default Calculator;
+
