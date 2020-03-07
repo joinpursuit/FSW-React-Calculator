5200a9b6a15af0cef7f430f79b06c387b416b461
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-03-07 18:55:06 -0500

    displaying values on calc

--- my-calculator-app/src/Componenets/Calculator.js
+++ my-calculator-app/src/Componenets/Calculator.js
@@ -1,25 +1,27 @@
 import React, {Component} from "react";
+// import ClearButton from "../Componenets/ClearButton"
+// import * as math from 'mathjs'
+// import DisplayCalc from "../Componenets/DisplayCalc"
 
 import "../CSS/styleCalc.css";
 
+
 class Calculator extends Component {
   // constructor(props) {
-    // super(props)
+  //   super(props)
     state = {
-      displayValue: 0,
+      displayValue: "0",
       previousValue: null,
       operator: null,
       newValue: false
-    // }
-  }
-
-
+    }
+ 
   handleNumber = (e) => {
     const { displayValue, newValue } = this.state;
     if(displayValue === "0" || newValue === true) {
       this.setState({
         displayValue: e.target.value
-      })
+      });
     } else {
       this.setState({
         displayValue: displayValue + e.target.value
@@ -28,47 +30,47 @@ class Calculator extends Component {
   }
 
   render () {
-    let {displayValue} = this.state
+    // let {displayValue} = this.state
     
     
-    console.log(this.state)
-    return (
-      <div className="clacBox">
-      
-      <div Display displayValue> {displayValue} </div>
-        
+    // console.log(this.state)
+  return(
+    <div className="calcBox">
+      <div className="DisplayCalc"> 
+      <p> {this.state.displayValue}  </p>
+      </div>
         <div className="row"> 
-        <button > ÷ </button>
-        <button > % </button>
-        <button > ± </button>
-        <button > C </button>
+        <button value="÷" onClick={this.handleNumber}> ÷ </button>
+        <button value="%" onClick={this.handleNumber}> % </button>
+        <button value="±" onClick={this.handleNumber}> ± </button>
+        <button value="AC" onClick={this.handleNumber}> AC </button> 
         </div>
 
         <div className="row">
-        <button > * </button>
-        <button > 9 </button>
-        <button > 8 </button>
-        <button > 7 </button>
+        <button value="*" onClick={this.handleNumber}> * </button>
+        <button value="9" onClick={this.handleNumber}> 9 </button>
+        <button value="8" onClick={this.handleNumber}> 8 </button>
+        <button value="7" onClick={this.handleNumber}> 7 </button>
         </div> 
 
         <div className="row"> 
-        <button > - </button>
-        <button > 6 </button>
-        <button > 5 </button>
-        <button > 4 </button>
+        <button value="-" onClick={this.handleNumber}> - </button>
+        <button value="6" onClick={this.handleNumber}> 6 </button>
+        <button value="5" onClick={this.handleNumber}> 5 </button>
+        <button value="4" onClick={this.handleNumber}> 4 </button>
         </div>
 
         <div className="row">
-        <button > + </button>
-        <button > 3 </button>
-        <button > 2 </button>
-        <button > 1 </button>
+        <button value="+" onClick={this.handleNumber}> + </button>
+        <button value="3" onClick={this.handleNumber}> 3 </button>
+        <button value="2" onClick={this.handleNumber}> 2 </button>
+        <button value="1" onClick={this.handleNumber}> 1 </button>
         </div>
 
         <div className="row"> 
-        <button > = </button>
-        <button > . </button>
-        <button > 0 </button>
+        <button value="=" onClick={this.handleNumber}> = </button>
+        <button value="." onClick={this.handleNumber}> . </button>
+        <button value="0" onClick={this.handleNumber}> 0 </button>
         </div>
       </div>
     )
