f3bae1b88af475705bf93c83811a317f6e7a4291
cassidybeni <cassidybeni@pursuit.org>
2021-04-19 22:37:06 -0400

    Set values of buttons

---
+++ calculator-app/src/Components/Screen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class Screen extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" defaultValue="0"></input>
+            </div>
+        )
+    }
+}
+
+export default Screen
