9688fa6436b6b057931b4e9c380519858e3a505a
Farrah Isabelle Rios <isabellefarrah80@gmail.com>
2021-04-23 13:25:17 -0400

    add plusMinus operator so that it negates the number in the input

--- calcculator-app/src/components/Input.js
+++ calcculator-app/src/components/Input.js
@@ -11,10 +11,12 @@ class Input extends React.Component {
     mod: "",
     secondDisplay: [],
     answer: 0,
+    // equal:
   };
 
   handleInputChange = (event) => {
     //let display = event.target.value.split("");
+
     if (this.state.mod) {
       const { name, value } = event.target; //this makes the state update with each change. Not quite sure why. It was part of forms lesson. I think its deconstructing in some way.
       this.setState({ [name]: value }); // this is part of that^^
@@ -37,73 +39,119 @@ class Input extends React.Component {
   handleGrid = (event) => {
     if (this.state.mod) {
       this.setState((prevState) => ({
-        input: [...prevState.input,...[event]].join(''),
-        secondDisplay: [...prevState.input,...[event]].join(''),
+        input: [prevState.input, ...[event]].join(""),
+        secondDisplay: [prevState.input, ...[event]].join(""),
       }));
     } else {
       this.setState((prevState) => ({
-        input: [...prevState.input,...[event]].join(''),
-        firstDisplay: [...prevState.input,...[event]].join(''),
+        input: [...prevState.input, ...[event]].join(""),
+        firstDisplay: [...prevState.input, ...[event]].join(""),
       }));
     }
     console.log(this.state);
-    
   };
 
   handleMod = (event) => {
-    console.log(event);
-    this.setState((prevState) => ({
-      input: "",
-      mod: event,
-      previous: prevState,
-    }));
+
+    
+    if(event === "plusMinus"){
+        this.setState((prevState) => ({
+          input: Number(prevState.input) * -1,
+          firstDisplay: Number(prevState.input) * -1,
+         mod: event,
+          previous: prevState, // no real functionality just wanted to call prevState so it wouldnt stay faded
+        }));    
+console.log(this.state)
+    }else{
+
+        this.setState((prevState) => ({
+          input: "",
+          // firstDisplay: [...[answer]].join(""),
+         mod: event,
+          previous: prevState, // no real functionality just wanted to call prevState so it wouldnt stay faded
+        }));
+    }
+
+      }
+ 
+    
+
+ 
+
    
-  };
 
   handleEqual = (event) => {
     console.log(event);
+    // if(event.second){
     if (event.mod === "sum") {
       let answer = Number(event.first) + Number(event.second);
       this.setState((prevState) => ({
-        input: [...[answer]].join(''),
-        firstDisplay: [...[answer]].join(''),
+        //mimicked first conditional of handleGrid in order for state to make answer first display which resets state each time and allows for cumulative functionality
+        input: [...[answer]].join(""),
+        firstDisplay: [...[answer]].join(""),
         previous: prevState,
+        mod:""
       }));
     }
     if (event.mod === "subtract") {
       let answer = Number(event.first) - Number(event.second);
       this.setState((prevState) => ({
-        input: [...[answer]].join(''),
-        firstDisplay: [...[answer]].join(''),
+        input: [...[answer]].join(""),
+        firstDisplay: [...[answer]].join(""),
         previous: prevState,
+        mod:""
       }));
     }
     if (event.mod === "divide") {
       let answer = Number(event.first) / Number(event.second);
       this.setState((prevState) => ({
-        input: [...[answer]].join(''),
-        firstDisplay: [...[answer]].join(''),
+        input: [...[answer]].join(""),
+        firstDisplay: [...[answer]].join(""),
         previous: prevState,
+        mod:""
       }));
     }
     if (event.mod === "multiply") {
       let answer = Number(event.first) * Number(event.second);
       this.setState((prevState) => ({
-        input: [...[answer]].join(''),
-        firstDisplay: [...[answer]].join(''),
+        input: [...[answer]].join(""),
+        firstDisplay: [...[answer]].join(""),
         previous: prevState,
+        mod:""
       }));
     }
-    if (event.mod === "plusMinus") {
-      let answer = Number(event.first) * Number(event.second);
-      this.setState((prevState) => ({
-        input: [...[answer]].join(''),
-        firstDisplay: [...[answer]].join(''),
-        previous: prevState,
-      }));
-    }
+  // }else{
+  //   let answer = Number(event.first);
+  //   this.setState((prevState) => ({
+  //     input: [...[answer]].join(""),
+  //     firstDisplay: [...[answer]].join(""),
+  //     previous: prevState,
+  //   }));
+  }
+    
+  
+  
+  
+  // if (event.mod === "plusMinus" && event.second === "") {
+  //     let answer = event.first 
+  //     this.setState((prevState) => ({
+  //       input: [...[answer]].join(""),
+  //       firstDisplay: [...[answer]].join(""),
+  //       previous: prevState,
+  //     }));
+  //   } else{
+  //   let answer = Number(event.first);
+  //   this.setState((prevState) => ({
+  //     input: [...[answer]].join(""),
+  //     firstDisplay: [...[answer]].join(""),
+  //     previous: prevState,
+  //   }));
 
-  };
+  // }
+
+
+
+  
 
   render() {
     return (
@@ -118,7 +166,7 @@ class Input extends React.Component {
         <button onClick={this.test}></button>
         <Grid handleGrid={this.handleGrid} />
         <Mod handleMod={this.handleMod} />
-        <Equal
+        <Equal //will loop values of state properties by making a copy of them in child component and passing in as arguments back to parent function
           first={this.state.firstDisplay}
           mod={this.state.mod}
           second={this.state.secondDisplay}
