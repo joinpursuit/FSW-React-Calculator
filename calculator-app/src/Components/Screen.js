19372f11a6d615614dd61498af1a190ab6a2ab16
cassidybeni <cassidybeni@pursuit.org>
2021-04-25 15:50:36 -0400

    Add styling

--- calculator-app/src/Components/Screen.js
+++ calculator-app/src/Components/Screen.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react"
+import "./Screen.css"
 
 export class Screen extends Component {
   render() {
     const { displayValue } = this.props
     return (
-      <div>
-        <input type="text" readOnly value={displayValue}></input>
+      <div className="screen-container">
+        <input className="screen" type="text" readOnly value={displayValue}></input>
       </div>
     )
   }
