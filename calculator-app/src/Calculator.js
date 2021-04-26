93169b2444ed08b827da641fe65690f8960e3298
Coreen Cooper <coreencooper@pursuit.org>
2021-04-26 16:05:36 -0400

    add buttons

---
+++ calculator-app/src/Calculator.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Calculator = () => {
+    return (
+<section>
+    <p id="display"></p>
+    <button className="button">1</button>
+    <button className="button">2</button>
+    <button className="button">3</button>
+    <button className="button">4</button>
+    <button className="button">5</button>
+    <button className="button">6</button>
+    <button className="button">7</button>
+    <button className="button">8</button>
+    <button className="button">9</button>
+    <button className="button">0</button>
+    <button className="button">+</button>
+    <button className="button">-</button>
+    <button className="button">÷</button>
+    <button className="button">=</button>
+    <button className="button">x</button>
+</section>
+    )
+}
+
+export default Calculator;
