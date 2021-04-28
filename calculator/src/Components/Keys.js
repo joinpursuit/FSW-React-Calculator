9ee94077daa271560ae3bccded9861d7715b81d7
Daliz Cruz <dalizcruz@gmail.com>
2021-04-28 18:35:46 -0400

    negative sign feature added

--- calculator/src/Components/Keys.js
+++ calculator/src/Components/Keys.js
@@ -1,13 +1,19 @@
 import { Component } from "react";
 
 class Keys extends Component {
-    constructor(props){
-        super(props)
-        this.state = {display: ""}
-    }
+        state = {display: ""}
 
-  input = () => {
-         
+
+  input = (e) => {
+      this.setState((prevState) => ({display: (prevState.display + e.target.name)}))
+  }
+
+  sign = (e) => {
+      const { name } = e.target
+      const { display } = this.state
+      if(name === "+/-"){
+          this.setState({display: display * -1})
+      }
   }
 
   render() {
@@ -20,34 +26,34 @@ class Keys extends Component {
 
           <div className="row-one">
           <button onClick={this.input}>AC</button>
-          <button onClick={this.input}>+/-</button>
-          <button onClick={this.input}>/</button>
-          <button onClick={this.input}>x</button>
+          <button onClick={this.sign} name="+/-">+/-</button>
+          <button onClick={this.input} name="%">%</button>
+          <button onClick={this.input} name="/">/</button>
           </div>
         <div className="row-two">
-          <button onClick={this.input}>7</button>
-          <button onClick={this.input}>8</button>
-          <button onClick={this.input}>9</button>
-          <button onClick={this.input}>x</button>
+          <button onClick={this.input} name="7">7</button>
+          <button onClick={this.input} name="8" >8</button>
+          <button onClick={this.input} name="9">9</button>
+          <button onClick={this.input} name="x">x</button>
         </div>
 
         <div className="row-three">
-          <button onClick={this.input}>4</button>
-          <button onClick={this.input}>5</button>
-          <button onClick={this.input}>6</button>
-          <button onClick={this.input}>-</button>
+          <button onClick={this.input} name="4">4</button>
+          <button onClick={this.input} name="5">5</button>
+          <button onClick={this.input} name="6">6</button>
+          <button onClick={this.input} name="-">-</button>
         </div>
         <div className="row-four">
-          <button onClick={this.input}>1</button>
-          <button onClick={this.input}>2</button>
-          <button onClick={this.input}>3</button>
-          <button onClick={this.input}>+</button>
+          <button onClick={this.input} name="1">1</button>
+          <button onClick={this.input} name="2">2</button>
+          <button onClick={this.input} name="3">3</button>
+          <button onClick={this.input} name="+">+</button>
         </div>
 
         <div className="row-five">
-          <button onClick={this.input}>0</button>
-          <button onClick={this.input}>.</button>
-          <button onClick={this.input}>=</button>
+          <button onClick={this.input} name="0">0</button>
+          <button onClick={this.input} name=".">.</button>
+          <button onClick={this.input} name="=">=</button>
         </div>
       </section>
     );
