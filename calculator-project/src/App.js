61083522b3f81923a2af27122ab70043259bb91a
Yashiry Azize <azizeyashiry@gmail.com>
2021-04-28 17:12:55 -0400

    fixed buttons and +/- button working

--- calculator-project/src/App.js
+++ calculator-project/src/App.js
@@ -5,20 +5,22 @@ import KeyPad from "./Components/KeyPad";
 class App extends React.Component {
   state = {result: ""}
 
-  handleNum = (button) => {
-    this.setState({ result: this.state.result + button });
+  handleNum = (value) => {
+    this.setState({ result: this.state.result + value});
   }
 
-  // handleOperator = () => {
-    
-  // }
+  handleOperator = (value) => {
+    if (value === "+/-") {
+      this.setState({result: this.state.result * -1})
+    }
+  }
 
 
   render() {
     const {result} = this.state
     return (
       <div className="App">
-        <KeyPad handleNum={this.handleNum} result={result}/>
+        <KeyPad handleOperator={this.handleOperator} handleNum={this.handleNum} result={result}/>
       </div>
     );
   }
--- calculator-project/src/Components/KeyPad.js
+++ calculator-project/src/Components/KeyPad.js
@@ -2,27 +2,27 @@ import React from "react"
 import Solution from "./Solution"
 
 
-const KeyPad = ({result, handleNum}) => {
+const KeyPad = ({result, handleNum, handleOperator}) => {
     return (
         <section className="container">
             <Solution result={result}/>
-            {/* <input type="button" onClick={this.handleOperator} className="child key10" value="AC"/>
-            <input type="button" onClick={this.handleOperator} className="child key11" value="+/-"/>
-            <input type="button" onClick={this.handleOperator} className="child key12" value="÷"/> */}
-            <input type="button" onClick={e => handleNum(e.target.value)} className="child key7" value="7"/>
-            <input type="button" onClick={e => handleNum(e.target.value)} className="child key8" value="8"/>
-            <input type="button" onClick={e => handleNum(e.target.value)} className="child key9" value="9"/>
-            {/* <input type="button" onClick={this.handleOperator} className="child key13" value="x"/> */}
-            <input type="button" onClick={e => handleNum(e.target.value)} className="child key4" value="4"/>
-            <input type="button" onClick={e => handleNum(e.target.value)} className="child key5" value="5"/>
-            <input type="button" onClick={e => handleNum(e.target.value)} className="child key6" value="6"/>
-            {/* <input type="button" onClick={this.handleOperator} className="child key14" value="-"/>
-            <input type="button" onClick={this.handleOperator} className="child key15" value="+"/> */}
-            <input type="button" onClick={e => handleNum(e.target.value)} className="child key1" value="1"/>
-            <input type="button" onClick={e => handleNum(e.target.value)} className="child key2" value="2"/>
-            <input type="button" onClick={e => handleNum(e.target.value)} className="child key3" value="3"/>
-            {/* <input type="button" onClick={this.handleOperator} className="child key16" value="="/> */}
-            <input type="button" onClick={e => handleNum(e.target.value)} className="child key0" value="0"/>
+            <button className="child key10" value="AC">AC</button>
+            <button onClick={e => handleOperator(e.target.value)} className="child key11" value="+/-">+/-</button>
+            <button className="child key12" value="÷">÷</button>
+            <button onClick={e => handleNum(e.target.value)} className="child key7" value="7">7</button>
+            <button onClick={e => handleNum(e.target.value)} className="child key8" value="8">8</button>
+            <button onClick={e => handleNum(e.target.value)} className="child key9" value="9">9</button>
+            <button className="child key13" value="x">x</button>
+            <button onClick={e => handleNum(e.target.value)} className="child key4" value="4">4</button>
+            <button onClick={e => handleNum(e.target.value)} className="child key5" value="5">5</button>
+            <button onClick={e => handleNum(e.target.value)} className="child key6" value="6">6</button>
+            <button className="child key14" value="-">-</button>
+            <button className="child key15" value="+">+</button>
+            <button onClick={e => handleNum(e.target.value)} className="child key1" value="1">1</button>
+            <button onClick={e => handleNum(e.target.value)} className="child key2" value="2">2</button>
+            <button onClick={e => handleNum(e.target.value)} className="child key3" value="3">3</button>
+            <button className="child key16" value="=" >=</button>
+            <button onClick={e => handleNum(e.target.value)} className="child key0" value="0">0</button>
         </section>
     )
 }
