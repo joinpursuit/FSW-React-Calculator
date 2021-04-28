1e240e61092e30b639beec8f2a9ae368a86401e6
Yesenia L <ylopezcr123@gmail.com>
2021-04-28 14:01:15 -0400

    inprogress

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,69 +1,75 @@
-
-import React, { Component } from 'react'
-import Tot from './Total'
-import Cal from './Calculator'
-import './App.css';
-//import { render } from '@testing-library/react';
-
-
+import React, { Component } from "react";
+import Tot from "./Total";
+import Cal from "./Calculator";
+import "./App.css";
 
 export default class App extends Component {
-
-//results beggining stage
-state = {
-  result:''
- 
-}
-//results after number clicked
-select = buttonN => {
-  if (buttonN === '=') {
-    this.math();
-  } else if (buttonN === 'Clear') {
-    this.reset()
-  } else if (buttonN === 'Delete') {
-    this.delete()
-  }else
-  this.setState({
-      result: this.state.result + buttonN
-  });
-}
-
-
-//delete
-delete = () => {
-this.setState({
-  result: this.state.result.slice(0, -1)
-})
-}
-
-//reset
-reset = () => {
-  this.setState({
-   result:'0'
-  })
-}
-//math
-math = () => {
-  try {this.setState({
-    result:eval(this.state.result)
-  });
-} catch(e)  {
+  //results beggining stage
+  state = {
+    result: "",
+  };
+  //results after number clicked
+  select = (buttonN) => {
+    if (buttonN === "=") {
+      this.math();
+    } else if (buttonN === "AC") {
+      this.reset();
+    } else if (buttonN === "Delete") {
+      this.delete();
+    } else if (buttonN === "%") {
+      this.percent();
+    } else if (buttonN === "+/-") {
+      this.posneg();
+    } else
+      this.setState({
+        result: this.state.result + buttonN,
+      });
+  };
+  //positive and negitive intigers
+  posneg = () => {
     this.setState({
-
-      result: 'error'
+      result:  Math.abs(-this.state.result)
     });
-}
-}
-render() {
+  };
+  //percent
+  percent = () => {
+    this.setState({
+      result: this.state.result / 100,
+    });
+  };
+  //delete
+  delete = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1),
+    });
+  };
 
-  return (
-    <div className="App">
-      <div className='calc'>
-     
-      <Tot result={this.state.result}/>
-      <Cal select={this.select}/>
+  //reset
+  reset = () => {
+    this.setState({
+      result: "",
+    });
+  };
+  //math
+  math = () => {
+    try {
+      this.setState({
+        result: eval(this.state.result),
+      });
+    } catch (e) {
+      this.setState({
+        result: "error",
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="calc">
+          <Tot result={this.state.result} />
+          <Cal select={this.select || 0} />
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
--- react-calculator/src/Calculator.js
+++ react-calculator/src/Calculator.js
@@ -1,41 +1,100 @@
-import React, { Component } from 'react'       
+import React, { Component } from "react";
 //import React from 'react'
 
 export default class Cal extends Component {
-    select = e => {
-        this.props.select(e.target.name);
-    }
-    render() {
-        return (
-            <div className='button'>
-                 <button name= 'Clear' onClick={this.select}> Clear </button>
-                 <button name= '+/-' onClick={this.select}> +/-</button>
-                 <button name= '%' onClick={this.select}> % </button>
-                 <button name= '/' onClick={this.select}> / </button>
+  select = (e) => {
+    this.props.select(e.target.name);
+  };
+  render() {
+    return (
+      <div className="button">
+        <button className= 'hotpink' name="AC" onClick={this.select}>
+          {" "}
+          AC{" "}
+        </button>
+        <button className='pink' name="+/-" onClick={this.select}>
+          {" "}
+          +/-
+        </button>
+        <button className= 'pink' name="%" onClick={this.select}>
+          {" "}
+          %{" "}
+        </button>
+        <button className='pink' name="/" onClick={this.select}>
+          {" "}
+          /{" "}
+        </button>
 
-                 <br/>
+        <br />
 
-                <button name= '7' onClick={this.select}> 7 </button>
-                <button name= '8' onClick={this.select}> 8 </button>
-                <button name= '9' onClick={this.select}> 9 </button>
-                <button name= '*' onClick={this.select}> * </button>
-                <br/>
-                <button name= '4' onClick={this.select}> 4 </button>
-                <button name= '5' onClick={this.select}> 5 </button>
-                <button name= '6' onClick={this.select}> 6 </button>
-                <button name= '-' onClick={this.select}> - </button>
-                <br/>
-                <button name= '1' onClick={this.select}> 1 </button>
-                <button name= '2' onClick={this.select}> 2 </button>
-                <button name= '3' onClick={this.select}> 3 </button>
-                <button name= '+' onClick={this.select}> + </button>    
-                <br/>
-                <button name= '0' onClick={this.select}> 0 </button>
-                <button name= '.' onClick={this.select}> . </button>
-                <button name= 'Delete' onClick={this.select}> Delete </button>
-                <button name= '=' onClick={this.select}> = </button>
-            
-            </div>
-        )
-    }
+        <button name="7" onClick={this.select}>
+          {" "}
+          7{" "}
+        </button>
+        <button name="8" onClick={this.select}>
+          {" "}
+          8{" "}
+        </button>
+        <button name="9" onClick={this.select}>
+          {" "}
+          9{" "}
+        </button>
+        <button className='pink' name="*" onClick={this.select}>
+          {" "}
+          *{" "}
+        </button>
+        <br />
+        <button name="4" onClick={this.select}>
+          {" "}
+          4{" "}
+        </button>
+        <button name="5" onClick={this.select}>
+          {" "}
+          5{" "}
+        </button>
+        <button name="6" onClick={this.select}>
+          {" "}
+          6{" "}
+        </button>
+        <button className='pink' name="-" onClick={this.select}>
+          {" "}
+          -{" "}
+        </button>
+        <br />
+        <button name="1" onClick={this.select}>
+          {" "}
+          1{" "}
+        </button>
+        <button name="2" onClick={this.select}>
+          {" "}
+          2{" "}
+        </button>
+        <button name="3" onClick={this.select}>
+          {" "}
+          3{" "}
+        </button>
+        <button className='pink' name="+" onClick={this.select}>
+          {" "}
+          +{" "}
+        </button>
+        <br />
+        <button name="0" onClick={this.select}>
+          {" "}
+          0{" "}
+        </button>
+        <button name="." onClick={this.select}>
+          {" "}
+          .{" "}
+        </button>
+        <button name="Delete" onClick={this.select}>
+          {" "}
+          Delete{" "}
+        </button>
+        <button className ='hotpink' name="=" onClick={this.select}>
+          {" "}
+          ={" "}
+        </button>
+      </div>
+    );
+  }
 }
