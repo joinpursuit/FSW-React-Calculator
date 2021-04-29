cde64425f0f53a0b0a29c3595febb6d9acc18235
Jordan Bobadilla <jordanboba08@gmail.com>
2021-04-28 22:03:26 -0400

    Commas and posAnaNeg done

--- react-calculator/src/components/Calculator.js
+++ react-calculator/src/components/Calculator.js
@@ -6,15 +6,15 @@ class Calculator extends Component {
   state = { input: "" };
 
   handleNumbers = (e) => {
-    this.setState((prevState) => ({ input: formatNums(prevState.input + e.target.value)}));
+    this.setState((prevState) => ({ input: prevState.input + e.target.value}));
   };
 
   handleOperators = (e) => {
     const { value } = e.target
     const { input } = this.state
     if(value === "+/-") {
-        this.setState({input: input * -1})
-    }
+        this.setState({input: Number(input) * -1})
+    } 
   };
 
   handleSubmit = (e) => {
@@ -27,32 +27,32 @@ class Calculator extends Component {
       <section className="wrapper">
         <section className="Calculator">
           <form onSubmit={this.handleSubmit}>
-            <input type="text" value={input || 0} readOnly={true}/>
-            <div className="one">
+            <input type="text" max="13" value={formatNums(input.toString()) || 0} readOnly={true}/>
+            <div>
               <input type="button" value="AC" />
               <input type="button" value="+/-" onClick={this.handleOperators}/>
               <input type="button" value="%" />
               <input type="button" value="/" />
             </div>
-            <div className="two">
+            <div>
               <input type="button" value="7" onClick={this.handleNumbers} />
               <input type="button" value="8" onClick={this.handleNumbers} />
               <input type="button" value="9" onClick={this.handleNumbers} />
               <input type="button" value="*" />
             </div>
-            <div className="three">
+            <div>
               <input type="button" value="4" onClick={this.handleNumbers} />
               <input type="button" value="5" onClick={this.handleNumbers} />
               <input type="button" value="6" onClick={this.handleNumbers} />
               <input type="button" value="-" />
             </div>
-            <div className="four">
+            <div>
               <input type="button" value="1" onClick={this.handleNumbers} />
               <input type="button" value="2" onClick={this.handleNumbers} />
               <input type="button" value="3" onClick={this.handleNumbers} />
               <input type="button" value="+" />
             </div>
-            <div className="five">
+            <div>
               <input type="button" value="0" onClick={this.handleNumbers} />
               <input type="button" value="00" onClick={this.handleNumbers} />
               <input type="button" value="." />
