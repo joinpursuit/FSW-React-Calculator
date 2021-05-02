0cccd9b9a1324473a0ad74947ef06ea00b6c3049
joshuamendez <joshuafmendez@gmail.com>
2021-05-01 22:38:01 -0400

    implemented Button and Input components, and stesting out mathjs

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-// const isOperator = val => {
-//     return !isNaN(val) || val === '.' || val === '=';
-// }
-
 const Button = ({icon, handleClick}) => {
     return (
         <button onClick={() => handleClick({icon})}>{icon}</button>
