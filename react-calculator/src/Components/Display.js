074cd1a7eecc085d09b47124203bf134e1393df3
Durdona Djalilova <ddjalilova3@gmail.com>
2021-05-05 20:48:17 -0400

    final

--- react-calculator/src/Components/Display.js
+++ react-calculator/src/Components/Display.js
@@ -5,11 +5,7 @@ class Display extends React.Component {
   handleDisplay = () => {
     const { showNums, isPositive, result, numbers } = this.props;
     if (result === "") {
-      // if (numbers.length === 0 || Number(showNums) === 0) {
-      //   return 0;
-      // } else {
-        return showNums.toLocaleString();
-      // }
+      return showNums.toLocaleString();
     } else {
       return Number(result).toLocaleString();
     }
