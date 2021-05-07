77b99cb77a7d2cce3f58e2f4c00c9bb49524f325
Kobe <tahiriadam@gmail.com>
2021-05-07 03:33:20 -0400

    fixed clear functionality

--- calculator_project/src/App.js
+++ calculator_project/src/App.js
@@ -3,77 +3,123 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-
   state = { input: "", prevNum: "", startNewNum: true, operator: "" };
 
   handleButton = (e) => {
-    const {value} = e.target
-      if(this.state.startNewNum) {
-        this.setState({input: value, startNewNum: false})
-      } else {
-        console.log("else")
-        this.setState({input: this.state.input + value})
-      }
+    const { value } = e.target;
+    if (this.state.startNewNum) {
+      this.setState({ input: value, startNewNum: false });
+    } else {
+      console.log("else");
+      this.setState({ input: this.state.input + value });
+    }
   };
 
   handlePosNeg = (e) => {
-    const {value} = e.target
-    const {input} = this.state
-    this.setState({input: input * -1})
-  }
+    const { value } = e.target;
+    const { input } = this.state;
+    this.setState({ input: input * -1 });
+  };
 
   handleZero = (e) => {
-    const {value} = e.target;
-    const {input} = this.state;
-    if(input !== "") {
-      this.setState({input: input + value})
-    } 
-  }
-  
+    const { value } = e.target;
+    const { input } = this.state;
+    if (input !== "") {
+      this.setState({ input: input + value });
+    }
+  };
+
   handleOperation = (e) => {
-    const {value} = e.target;
-    this.setState({startNewNum: true, prevNum: this.state.input, operator: value})
-  }
+    const { value } = e.target;
+    this.setState({
+      startNewNum: true,
+      prevNum: this.state.input,
+      operator: value,
+    });
+  };
 
   handleDecimal = (e) => {
-    const {value} = e.target;
-    const {input} = this.state;
-    if(!input.includes(value)) {
-      this.setState({input: input + value })
+    const { value } = e.target;
+    const { input } = this.state;
+    if (!input.includes(value)) {
+      this.setState({ input: input + value });
     }
-  }
+  };
 
   handlePercentage = (e) => {
-    const {value} = e.target;
-    this.setState({input: this.state.input / 100})
-  }
+    const { value } = e.target;
+    this.setState({ input: this.state.input / 100 });
+  };
 
   handleClear = (e) => {
-    const {value} = e.target;
-    if(value === "A/C") {
-      this.setState({input: "", prevNum: "", operator: "", startNewNum: true})
-    } if(value === "C") {
-      this.setState({input: "", prevNum: "", operator: "", startNewNum: true})
+    const { value } = e.target;
+    const { input, prevNum, operator } = this.state;
+    if (value === "A/C") {
+      this.setState({
+        input: "",
+        prevNum: "",
+        operator: "",
+        startNewNum: true,
+      });
     }
-  }
+    if (value === "C") {
+      if (operator === "") {
+        this.setState({
+          input: "",
+          prevNum: "",
+          operator: "",
+          startNewNum: true,
+        });
+      } else if (input !== "" && prevNum !== "" && operator !== "") {
+        this.setState({
+          input: "",
+          prevNum: prevNum,
+          operator: operator,
+          startNewNum: true,
+        });
+      }
+    }
+  };
 
   calculate = (e) => {
-    const {value} = e.target;
-    if(this.state.operator === "+") {
-      this.setState({input: parseFloat(this.state.input) + parseFloat(this.state.prevNum), startNewNum: true})
-    } else if(this.state.operator === "-") {
-      this.setState({input:  parseFloat(this.state.prevNum) - parseFloat(this.state.input), startNewNum: true})
-    } else if(this.state.operator === "x") {
-      this.setState({input:  parseFloat(this.state.prevNum) * parseFloat(this.state.input), startNewNum: true})
-    }else if(this.state.operator === "/") {
-      this.setState({input:  parseFloat(this.state.prevNum) / parseFloat(this.state.input), startNewNum: true})
+    const { value } = e.target;
+    if (this.state.operator === "+") {
+      this.setState({
+        input: parseFloat(this.state.input) + parseFloat(this.state.prevNum),
+        startNewNum: true,
+      });
+    } else if (this.state.operator === "-") {
+      this.setState({
+        input: parseFloat(this.state.prevNum) - parseFloat(this.state.input),
+        startNewNum: true,
+      });
+    } else if (this.state.operator === "x") {
+      this.setState({
+        input: parseFloat(this.state.prevNum) * parseFloat(this.state.input),
+        startNewNum: true,
+      });
+    } else if (this.state.operator === "/") {
+      this.setState({
+        input: parseFloat(this.state.prevNum) / parseFloat(this.state.input),
+        startNewNum: true,
+      });
     }
-  }
+  };
 
   render() {
     return (
       <div className="App">
-        <CalcButton handleButton={this.handleButton} handleClear={this.handleClear} handlePosNeg={this.handlePosNeg} handleOperation={this.handleOperation} input={this.state.input} handleDecimal={this.handleDecimal} handlePercentage={this.handlePercentage} handleZero={this.handleZero} calculate={this.calculate}/>
+        <CalcButton
+          handleButton={this.handleButton}
+          handleClear={this.handleClear}
+          handlePosNeg={this.handlePosNeg}
+          handleOperation={this.handleOperation}
+          input={this.state.input}
+          handleDecimal={this.handleDecimal}
+          handlePercentage={this.handlePercentage}
+          handleZero={this.handleZero}
+          calculate={this.calculate}
+        />
       </div>
     );
   }
