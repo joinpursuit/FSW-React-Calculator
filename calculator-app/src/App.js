1b29ab3b488a537d90bf262dfe447d830e703c74
cassidybeni <cassidybeni@pursuit.org>
2021-05-07 16:57:56 -0400

    Commas appear for large numbers

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -20,7 +20,7 @@ export class App extends Component {
   }
   
   handleNegative = () =>{
-    this.setState({ displayValue: String(this.state.displayValue.split(',').join("") * -1) })
+    this.setState({ displayValue: (numeral(this.state.displayValue.split(',').join("") * -1)).format('0,0') })
   }
   
   handleOperation = (e) => {
