52ebd72883d617a3a80e8081f73a63a0abce2a6c
Marsha Hall <mphall77@gmail.com>
2021-05-08 06:05:23 -0400

    finished for now

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-	state = { input: "", sequence: [] };
+	state = {
+		input: "",
+		prevNum: "",
+		currNum: "",
+		operator: "",
+		// sequence: [],
+	};
 
 	handleClick = (e) => {
-		this.setState({ input: e.target.value });
-		const updateSequence = () => {
-			this.setState((prevState) => ({
-				sequence: [...prevState.sequence, this.state.input],
-			}));
-		};
+		this.setState({ input: this.state.input + e.target.value });
 	};
 
 	handleAllClear = (e) => {
@@ -18,37 +19,117 @@ class App extends React.Component {
 	};
 
 	handleNegPos = () => {
-		this.setState({ input: Number(this.state.input) * -1 })
-	}
-
-	updateSequence = () => {
-		this.setState((prevState) => ({
-			sequence: [...prevState.sequence, this.state.input],
-		}));
+		this.setState({ input: Number(this.state.input) * -1 });
 	};
 
+	addZeroToInput = () => {
+		if (this.state.input !== "") {
+			this.setState({ input: this.state.input + 0 });
+		}
+	};
+
+	decimal = () => {
+		if (this.state.input.indexOf(".") === -1) {
+			this.setState({ input: this.state.input + "." });
+		}
+	};
+
+	add = () => {
+		this.setState({
+			prevNum: this.state.input,
+			input: "",
+			operator: "plus",
+		});
+	};
+
+	subtract = () => {
+		this.setState({
+			prevNum: this.state.input,
+			input: "",
+			operator: "subtract",
+		});
+	};
+
+	multiply = () => {
+		this.setState({
+			prevNum: this.state.input,
+			input: "",
+			operator: "multiply",
+		});
+	};
+
+	divide = () => {
+		this.setState({
+			prevNum: this.state.input,
+			input: "",
+			operator: "divide",
+		});
+	};
+
+	evaluate = () => {
+		if (this.state.input === "") {
+			this.setState({
+				input: 0,
+				prevNum: "",
+				operator: "",
+			});
+		} else if (this.state.operator === "plus") {
+			this.setState({
+				input: Number(this.state.prevNum) + Number(this.state.input),
+			});
+		} else if (this.state.operator === "subtract") {
+			this.setState({
+				input: Number(this.state.prevNum) - Number(this.state.input),
+			});
+		} else if (this.state.operator === "multiply") {
+			this.setState({
+				input: Number(this.state.prevNum) * Number(this.state.input),
+			});
+		} else if (this.state.operator === "divide") {
+			this.setState({
+				input: Number(this.state.prevNum) / Number(this.state.input),
+			});
+		}
+	};
+
+	// add to input string
+	// updateSequence = () => {
+	// 	this.setState((prevState) => ({
+	// 		sequence: [...prevState.sequence, this.state.input],
+	// 	}));
+	// };
+
 	render() {
-		const { input, sequence } = this.state;
+		const { input } = this.state;
 		return (
 			<div className="App">
 				<div className="calc-container ">
 					{/* D I S P L A Y S */}
 					<section id="display">
 						<div className="display">
-							{sequence.map((item) => {
+							{/* {sequence.map((item) => {
 								return <p>{item}</p>;
-							})}
+							})} */}
 							{/* <p>This is where the sequence displays</p> */}
 						</div>
-						<div className="display">{input}</div>
+						{/* add the comma to the input */}
+						<div className="display">
+							{new Intl.NumberFormat().format(input)}
+						</div>
 					</section>
 					{/* B U T T O N S */}
 					<section id="btn-container">
 						<div className="row">
-							<button onClick={this.handleAllClear}>AC</button>
-							<button onClick={this.handleNegPos}>+/-</button>
-							<button >%</button>
-							<button className="operator">&#247;</button>
+							<button onClick={this.handleAllClear} className="orange">
+								AC
+							</button>
+							<button onClick={this.handleNegPos} className="orange">
+								+/-
+							</button>
+							<button className="orange">%</button>
+							<button className="operator" onClick={this.divide}>
+								&#247;
+							</button>
 						</div>
 
 						<div className="row">
@@ -74,7 +155,9 @@ class App extends React.Component {
 							<button onClick={this.handleClick} value="6">
 								6
 							</button>
-							<button className="operator">-</button>
+							<button className="operator" onClick={this.subtract}>
+								-
+							</button>
 						</div>
 
 						<div className="row">
@@ -87,21 +170,23 @@ class App extends React.Component {
 							<button onClick={this.handleClick} value="3">
 								3
 							</button>
-							<button className="operator">+</button>
+							<button className="operator" onClick={this.add}>
+								+
+							</button>
 						</div>
 
 						<div className="row">
 							<button onClick={this.handleClick} value="0">
 								0
 							</button>
-							<button>.</button>
+							<button onClick={this.decimal}>.</button>
 							<button onClick={this.handleClick} value="00">
 								00
 							</button>
-							<button className="operator">=</button>
+							<button className="operator" onClick={this.evaluate}>
+								=
+							</button>
 						</div>
-
-						{/* C L E A R BUTTON */}
 					</section>
 				</div>
 			</div>
