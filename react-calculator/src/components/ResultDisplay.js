4870829be30d894629db56b71427e7ddff978e90
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-08-09 17:40:00 -0400

    add commas

--- react-calculator/src/components/ResultDisplay.js
+++ react-calculator/src/components/ResultDisplay.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 class ResultDisplay extends Component {
   render() {
     const { result } = this.props;
-    return <div id="result" className="row">{result}</div>;
+    // return <div id="result" className="row">{result}</div>;
+    return <div id="result" className="row">{Number(result).toLocaleString()}</div>;
   }
 }
 
