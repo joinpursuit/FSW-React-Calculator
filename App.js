99c37aeab3c04a2ab0dd16fcfa558ae5f6b03001
CarlosAMoran <Carlosamoran18@gmail.com>
2021-11-12 11:47:43 -0500

    State Created

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+    state = {
+        input: "",
+        previousNum: "",
+        currentNum: "",
+        operator: "",
+    }
+}
