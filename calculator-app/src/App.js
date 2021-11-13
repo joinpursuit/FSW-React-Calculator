5d03e91375d38297ffddd00311b177ab643ddb25
Theodore Frazier <theo.frazier@icloud.com>
2021-11-13 16:02:27 -0500

    buttons sucessfully imported and appearing

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,11 +1,62 @@
-import './App.css';
 
-function App() {
-  return (
-    <div>
-      Testing Fonts
-    </div>
-  );
+import './App.css';
+import './components/KeyPad'
+import KeyPad from './components/KeyPad';
+import {Component} from 'react'
+
+
+
+class App extends Component {
+
+  //Build Constructor 
+
+  constructor() {
+    super();
+    this.state = {
+      displaySays: 0
+    };
+  }
+
+  //To create 
+
+  onClick = button => {
+
+    if (button === "=") {
+      this.execute()
+    }
+
+    else if (button === "Clear") {
+      this.reset()
+    }
+
+    else if (button === "+/-") {
+      this.negative()
+    }
+
+    else {
+      this.setState({
+        displaySays: this.state.displaySays 
+      })
+    }
+
+  }
+
+  reset = () => {
+    this.setState({
+      displaySays: 0
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        Testing Fonts
+       <KeyPad />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
