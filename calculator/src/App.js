9bf1a2acb701e4ee5fd6d533d7c498c4b250f469
rae charles <r_charles87@yahoo.com>
2021-11-14 21:36:38 -0500

    built layout

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,11 +1,39 @@
+import { Component } from 'react';
 import './App.css';
+import data from './data/buttonData';
 
-function App() {
-  return (
-    <div> 
-      Hello World
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      digits: data.numbers,
+      result: 0
+    }
+  }
+
+  handleButtonDisplay=()=>{
+    
+  }
+  
+  render(){
+    console.log(this.state.digits)
+    let buttonVal = Object.keys(this.state.digits)
+    let numArr = this.state.digits.map((num)=>{
+      return <button className="button" id={num.value} value={num.value}>{num.digit}</button>
+      
+    })
+
+
+    return (
+      <div className= "calc-container">
+        <div className="display">{this.state.result}</div>
+        <div className="button-container">
+        {numArr}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ calculator/src/data/buttonData.js
@@ -0,0 +1,75 @@
+export default{
+   numbers: [
+      {
+       "digit": 0, 
+       "value": "zero",  
+      },
+      {
+         "digit": 1,
+         "value": "one"
+      },
+      {
+         "digit": 2,
+         "value": "two"
+      },
+      {
+         "digit": 3,
+         "value": "three"
+      },
+      {
+         "digit": 4,
+         "value": "four"
+      },
+      {
+         "digit": 5,
+         "value": "five"
+      },
+      {
+         "digit": 6,
+         "value": "six"
+      },
+      {
+         "digit": 7,
+         "value": "seven"
+      },
+      {
+         "digit": 8,
+         "value": "eight"
+      },
+      {
+         "digit": 9,
+         "value": "nine"
+      },
+      {
+         "digit": "=",
+         "value": "equal"
+      },
+      {
+         "digit": "AC",
+         "value": "clear"
+      },
+      {
+         "digit": "x" ,
+         "value": "multiply"
+      },
+      {
+         "digit": "+",
+         "value": "plus"
+      },
+      {
+         "digit": "-",
+         "value": "minus"
+      },
+      {
+         "digit": "÷",
+         "value": "divide"
+      },
+      {
+         "digit":"+/-" ,
+         "value": "pos-neg"
+      },
+      
+
+   ]
+}
+
