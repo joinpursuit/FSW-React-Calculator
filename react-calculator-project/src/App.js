a8738b72edc45c2ea3592a921ca189d7b708e25a
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-14 20:59:23 -0500

    removed Calculator component. Moved methods into App.js. Created props for Keypad.js

--- react-calculator-project/src/App.js
+++ react-calculator-project/src/App.js
@@ -1,12 +1,91 @@
 import { Component } from 'react';
 import './App.css';
-import Calculator from './components/Calculator';
+import Keypad from './components/Keypad';
 
-class App extends Component{
-  render(){
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      display: '0',
+      operation: '',
+      currentNum: 0,
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let makeEquation = this.state.display + this.state.operation + this.state.display; // adding strings together
+    
+    this.setState({
+      currentNum: makeEquation,
+    });
+  };
+
+  handleNumberClick = (event) => {
+    this.setState({
+      display: `${this.state.display + event.target.textContent}`,
+    });
+  };
+
+  handleOperationChange = (operand) => {
+    this.setState({
+      display:'0',
+      currentNum: Number(this.state.display),
+      operation: operand,
+    })
+  };
+
+  evaluate = () => {
+    const { display, currentNum, operation } = this.state;
+    const currentNum = this[operation](Number(currentNum), Number(display));
+    this.setState({
+      display: currentNum,
+    })  
+  }
+
+  handleIncrement = (num) => {
+    this.setState({
+      display: this.state.count + num,
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      display: '0',
+      operation: '',
+      result: 0,
+    });
+  };
+
+  handlePercent = (event) => {
+    console.log("Add code for handlePercent", event.target.value);
+  };
+  handleFlipSign = (event) => {
+    console.log("Add code for handleFlipSign", event.target.value);
+  };
+  handleDecimal = (event) => {
+    console.log("Add code for handleDecimal", event.target.value);
+  };
+
+  calculateEquals = () => {};
+
+  render() {
     return (
       <div className="App">
-        <Calculator />
+        <Keypad
+          handleSubmit={this.handleSubmit}
+          handleNumberClick={this.handleNumberClick}
+          handleOperationChange={this.handleOperationChange}
+          evaulate={this.evaluate}
+          handleIncrement={this.handleIncrement}
+          handleReset={this.handleReset}
+          handlePercent={this.handlePercent}
+          handleFlipSign={this.handleFlipSign}
+          handleDecimal={this.handleDecimal}
+          calculateEquals={this.calculateEquals}
+        />
       </div>
     );
   }
--- react-calculator-project/src/components/Keypad.js
+++ react-calculator-project/src/components/Keypad.js
@@ -2,17 +2,69 @@ import { Component } from "react";
 import './Keypad.css';
 
 class Keypad extends Component{
-    constructor(){
-        super();
-
-        this.state = {
-            // numInput: '',
-            selectedNum: 0,
-            operation: '',
-            nextSelectedNumber: '',
-            result: 3500,
+    render(){
+        // make buttons for 0-9
+        const zeroToNineButtons = () => {
+            const buttonArr = [];
+            for (let i = 9; i >=0; i--) {
+                buttonArr.push(
+                <button onClick={()=>this.props.handleNumberClick()} key={i} id={"n" + i} value={i}>{i}</button>
+                )};
+            return buttonArr;
         }
+
+        return(
+            <div className="keypad-container">
+                <div className="keypad-display">
+                    <div>{this.props.display}</div>
+                    <div>{this.props.currentNum}</div>
+                </div>
+                <div className="keypad-label">
+                  Hello Calculator
+                </div>
+                <form className="form-container">
+                    <div className="keypad-controls">
+                        <button 
+                            onClick={this.props.handleReset} 
+                            id="all-clear" 
+                            value="AC"
+                        >AC</button>
+                        <button 
+                            onClick={this.props.handleFlipSign}
+                            id="flip-sign" 
+                            value="+/-"
+                        >&#43;/&#45;</button>
+                        <button 
+                            onClick={this.props.handlePercent}
+                            id="percentage" 
+                            value="%"
+                        >&#37;</button>
+                    </div>
+                    <div className="keypad-numbers">
+                        { zeroToNineButtons() }
+                        <button onClick={this.props.handleDecimal} id="decimalpoint" value=".">.</button>
+                    </div>
+                    <div className="keypad-operators">
+                        <button onClick={()=>this.props.handleOperationChange('divide')} id="divide" value="/">&#247;</button>
+                        <button onClick={()=>this.props.handleOperationChange('multiply')} id="multiply" value="*">&times;</button>
+                        <button onClick={()=>this.props.handleOperationChange('subtract')} id="subtract" value="-">&#45;</button>
+                        <button onClick={()=>this.props.handleOperationChange('add')} id="add" value="+">&#43;</button> 
+                        <button onClick={this.props.evaluate} id="equals" value="=">&#61;</button>
+                    </div>
+                </form>
+                <div className="keypad-footer">
+                    <br />
+                </div>
+            </div>
+        )
     }
+    
+}
+
+export default Keypad;
+
+
+
 
     // handleCommas=()=>{
     //     /* START: FOR comma every 3 digits */
@@ -35,38 +87,6 @@ class Keypad extends Component{
     //     /* END: FOR comma every 3 digits */
     // }
 
-    handleSubmit=(event)=>{
-        event.preventDefault();
-        console.log("submit button pressed.")
-        let makeEquation = (this.state.selectedNum + this.state.operation + this.state.selectedNum) // adding strings together
-        console.log("makeEquation", makeEquation); // result is a string '3+3' and does not calculate
-        this.setState({
-            result: makeEquation,
-        })
-    }
-
-    handleNumberClick=(event)=>{
-        console.log("selected number is:", event.target.value); // to see the input in console as you click
-        // Store selected number
-        this.setState({
-            selectedNum: event.target.value,
-        })
-    }
-
-    handleOperationChange=(event)=>{
-        console.log("huh", event.target.value);
-        this.setState({
-            operation: event.target.value,
-        })
-    }
-
-    handleIncrement=(num)=>{
-        this.setState({
-            selectedNum: this.state.count +num,
-        })
-      }
-
-
     // calcSum=(arr)=>{
     //     let currentValue = 0;
     //     for (let num of arr) {
@@ -74,98 +94,8 @@ class Keypad extends Component{
     //     }
     //     return currentValue;
     // }
-    /* END: OPERATORS */
 
-    /* START: FORM - handle click events */
     // add to the display the number selected
     // set the state to have the 2 numbers with operator being added or whatever
-    /* END: FORM - handle click events */
 
-    handleReset=()=>{
-        this.setState({
-            // numInput: '',
-            selectedNum: 0,
-            operation: '',
-            nextSelectedNumber: '',
-            result: 0,
-        })
-        console.log("reset what this.state is using:", this.state);
-    }
-
-    handlePercent=(event)=>{
-        console.log("Add code for handlePercent", event.target.value);
-    }
-    handlePosNeg=(event)=>{
-        console.log("Add code for handlePosNeg", event.target.value);
-    }
-    handleDecimal=(event)=>{
-        console.log("Add code for handleDecimal", event.target.value);
-    }
-
-    calculateEquals=()=>{
-
-    }
-
-    render(){
-        // make buttons for 0-9
-        const zeroToNineButtons = () => {
-            const buttonArr = [];
-            for (let i = 9; i >=0; i--) {
-                buttonArr.push(
-                <button onClick={this.handleNumberClick} key={i} id={"n" + i} value={i} type="button">{i}</button>
-                )};
-            return buttonArr;
-        }
-
-        return(
-            <div className="keypad-container">
-                <div className="keypad-display">
-                    <div>{this.state.selectedNum}</div>
-                    <div>{this.state.result}</div>
-                </div>
-                <div className="keypad-label">
-                    {/* <p>••• Calculator 8.2 ••••••••••• (10-digit) •••</p> */}
-                </div>
-                <form onSubmit={this.handleSubmit} className="form-container">
-                    <div className="keypad-controls">
-                        <button 
-                            onClick={this.handleReset} 
-                            id="all-clear" 
-                            type="button" 
-                            value="AC"
-                        >AC</button>
-                        <button 
-                            onClick={this.handlePosNeg}
-                            id="pos-neg" 
-                            type="button" 
-                            value="+/-"
-                        >&#43;/&#45;</button>
-                        <button 
-                            onClick={this.handlePercent}
-                            id="percentage" 
-                            type="button" 
-                            value="%"
-                        >&#37;</button>
-                    </div>
-                    <div className="keypad-numbers">
-                        { zeroToNineButtons() }
-                        <button onClick={this.handleDecimal} id="decimalpoint" type="button" value=".">.</button>
-                    </div>
-                    <div className="keypad-operators">
-                        <button onClick={this.handleOperationChange} id="divide" type="button" value="/">&#247;</button>
-                        <button onClick={this.handleOperationChange} id="multiply" type="button" value="*">&times;</button>
-                        <button onClick={this.handleOperationChange} id="subtract" type="button" value="-">&#45;</button>
-                        <button onClick={this.handleOperationChange} id="add" type="button" value="+">&#43;</button> 
-                        <button id="equals" type="submit" value="=">&#61;</button>
-                    </div>
-                </form>
-                <div className="keypad-footer">
-                    <br />
-                </div>
-            </div>
-        )
-    }
     
-}
-
-export default Keypad;
