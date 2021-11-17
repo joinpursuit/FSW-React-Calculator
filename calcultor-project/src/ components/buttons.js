7ae4966f0be0242ca3cc652291fc2bf038a2d3e8
hinkabi <hinkabi@gmail.com>
2021-11-16 23:54:37 -0500

    changed buttons format

---
+++ calcultor-project/src/ components/buttons.js
@@ -0,0 +1,44 @@
+// import { Component } from "react"
+
+// class Buttons extends Component {
+//     constructor(){
+//         super()
+
+//         this.state = {
+//             numberButtons:[],
+//         }
+//     }
+
+
+// render(){
+//     const makeNumberButtons = () => {
+//         const numberButtons = [];
+//         for (let i = 0; i < 10; i++) {
+//           numberButtons.push(<button key={i}>{i}</button>);
+//         }
+//         return numberButtons;
+//       };
+  
+//     return(
+//     <div className="container">
+//           <form>
+//             <input type="text" placeholder="0" />
+//           </form>
+        
+//         <div className="calc-element">
+//           <button>-/+</button>
+//           <button>/</button>
+//           <button>*</button>
+//           <button>+</button>
+//           <button>-</button>
+//           <button>AC</button>
+//           <button>=</button>
+//           {/* <button>0</button> */}
+//           {makeNumberButtons()}
+//         </div>
+//       </div>
+//     );
+// }
+// }
+  
+// export default Buttons;
--- calcultor-project/src/App.js
+++ calcultor-project/src/App.js
@@ -1,36 +1,93 @@
 import "./App.css";
+import { Component } from "react";
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
 
-const makeNumberButtons = () => {
-  const numberButtons = [];
-  for(let i =1; i<10; i++){
-    numberButtons.push(
-      <button key={i}>{i}</button>
-    )
+    this.state = {
+      calcInput: ""
+    };
   }
-  return numberButtons
-}
 
-  return (
-    <div className="App">
-      <div className="calc-container">
-        <div className="calc-display"> 
-        Calc Display
-        </div>
-        <div className="operators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
-          <button>Delete</button>
-        </div>
-        <div className="numberButtons">
-          { makeNumberButtons() }
+
+  handleIncrement = (num) => {
+    this.setState({
+      calcInput: this.state.calcInput + num,
+    });
+  };
+
+  handleDecrement = (num) => {
+    this.setState({
+      calcInput: this.state.calcInput - num,
+    });
+  };
+
+  handleMultiply = (num) => {
+    this.setState({
+      count: this.state.count * num,
+    });
+  };
+
+  handleDivide = (num) => {
+    this.setState({
+      calcInput: this.state.calcInput / num,
+    });
+  };
+
+  handleInput = (event) => {
+    this.setState({
+      calcInput: event.target.value,
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      calcInput: 0,
+    });
+  };
+
+  render() {
+  
+    return (
+      <div className="App">
+        <div className="container">
+          <form>
+            <input
+              type="number"
+              placeholder="0"
+              value={this.state.calcInput}
+              onInput={this.handleInput}
+            />
+          </form>
+          <div className="calc-element">
+            <button>-/+</button>
+            <button onClick={this.handleDivide}>/</button>
+            <button onClick={this.handleMultiply}>*</button>
+            <button>=</button>
+            <button onClick={this.handleDecrement}>-</button>
+            <button onClick={this.handleIncrement}>+</button>
+            <button onClick={this.handleReset}>AC</button>
+
+            <button onClick={this.handleInput}>0</button>
+            <button onClick={this.handleInput}>1</button>
+            <button onClick={this.handleInput}>2</button>
+            <button onClick={this.handleInput}>3</button>
+            <button onClick={this.handleInput}>4</button>
+            <button onClick={this.handleInput}>5</button>
+            <button onClick={this.handleInput}>6</button>
+            <button onClick={this.handleInput}>7</button>
+            <button onClick={this.handleInput}>8</button>
+            <button onClick={this.handleInput}>9</button>
+            <button onClick={this.handleInput}>.</button>
+            
+            
+            
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
