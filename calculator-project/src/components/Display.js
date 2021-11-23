7e2f185d22f6efbb688e376f9618c78099f3dfb7
Name Ping Louie <namepinglouie@gmail.com>
2021-11-22 21:01:08 -0500

    handle features

--- calculator-project/src/components/Display.js
+++ calculator-project/src/components/Display.js
@@ -1,12 +1,18 @@
 import { Component } from "react";
 
 class Display extends Component {
+    inputCommas = (num) => {
+        return num.toLocaleString();
+        //return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    
     render() {
         let {result, num1, num2, displayOperator} = this.props;
+
         return (
             <div id="displaying-both">
                 <div className = "display-calculation">{num1}{displayOperator}{num2}</div>
-                <div className = "display-result">{result}</div>
+                <div className = "display-result">{this.inputCommas(result)}</div>
             </div>
         )
     }
--- calculator-project/src/components/Keypad.js
+++ calculator-project/src/components/Keypad.js
@@ -68,6 +68,7 @@ class Keypad extends Component{
     handleCalculation = () => {
         const {num1, num2, operator} = this.state;
         let total = 0;
+
         switch(operator) {
           case "add": total = this.operationAdd(num1, num2); break;
           case "subtract": total = this.operationSubtract(num1, num2); break;
@@ -85,21 +86,113 @@ class Keypad extends Component{
         })
       }
 
-      operationAdd = (num1, num2) => {
+    operationAdd = (num1, num2) => {
         return Number(num1) + Number(num2)
-      }
-    
-      operationSubtract = (num1, num2) => {
+    }
+
+    operationSubtract = (num1, num2) => {
         return num1 - num2;
-      }
-    
-      operationMultiply = (num1, num2) => {
+    }
+
+    operationMultiply = (num1, num2) => {
         return num1 * num2;
-      }
-    
-      operationDivide = (num1, num2) => {
+    }
+
+    operationDivide = (num1, num2) => {
         return num1 / num2;
-      }      
+    }      
+
+    featureClearAll = () => {
+        this.setState({
+          displayOperator: "",
+          isActiveOperator: false,
+          num1: "",
+          num2: "",
+          operator: "",
+          result: "0",
+        })
+    }
+    
+    featureClearCurrent = () => {
+        let {num1, num2, isActiveOperator} = this.state;
+        let str1 = num1;
+        let str2 = num2;
+
+        if(typeof num1 === "number") {
+            str1 = num1.toString();
+        }
+        if(typeof num2 === "number") {
+            str2 = num2.toString();
+        }
+
+        let newNum1 = str1.substring(0, str1.length - 1);
+        let newNum2 = str2.substring(0, str2.length - 1);
+
+        if(!isActiveOperator) {
+            this.setState({
+                num1: newNum1,
+            })
+        } else {
+            this.setState({
+                num2: newNum2,
+            })
+        }
+    }
+
+    featurePosNeg = (str1, str2) => {
+        let {isActiveOperator} = this.state;
+        let newNum1 = Number(str1) * -1;
+        let newNum2 = Number(str2) * -1;
+
+        if(!isActiveOperator) {
+            this.setState({
+                num1: newNum1,
+            })
+        } else {
+            this.setState({
+                num2: newNum2
+            })
+        }
+    }
+
+    featurePercent = () => {
+        let {num1, num2, isActiveOperator} = this.state;
+        let newNum1 = Number(num1);
+        let newNum2 = Number(num2);
+        let total = ((newNum2 / 100) * newNum1);
+
+        if(!isActiveOperator) {
+            this.setState({
+                num1: Number(num1) * .01,
+            })
+        } else {
+            this.setState({
+                num2: total,
+            })
+        }
+    }
+
+    featureDecimal = () => {
+        let {num1, num2, isActiveOperator} = this.state;
+        let newNum1 = num1 + ".";
+        let newNum2 = num2 + ".";
+
+        if(!isActiveOperator) {   
+            if(num1.includes(".")) return;
+            else {
+                this.setState({
+                    num1: newNum1,
+                })
+            }
+        } else {  
+            if(num2.includes(".")) return;
+            else {
+                this.setState({
+                    num2: newNum2,
+                })
+            }
+        }
+    }
 
     render() {
         let {result, num1, num2, displayOperator} = this.state;
