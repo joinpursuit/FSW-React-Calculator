874e4f107a457211d14c9a65615fb7e28382f98b
Name Ping Louie <namepinglouie@gmail.com>
2021-11-22 20:41:47 -0500

    step2: all buttons to be clickable

--- calculator-project/src/components/Display.js
+++ calculator-project/src/components/Display.js
@@ -2,10 +2,11 @@ import { Component } from "react";
 
 class Display extends Component {
     render() {
+        let {result, num1, num2, displayOperator} = this.props;
         return (
             <div id="displaying-both">
-                <div className = "display-calculation"></div>
-                <div className = "display-result"></div>
+                <div className = "display-calculation">{num1}{displayOperator}{num2}</div>
+                <div className = "display-result">{result}</div>
             </div>
         )
     }
--- calculator-project/src/components/Keypad.js
+++ calculator-project/src/components/Keypad.js
@@ -3,12 +3,24 @@ import buttons from "../data/buttonData";
 import Display from "./Display";
 
 class Keypad extends Component{
+    constructor() {
+        super();
+        this.state = {
+            result: "0",
+            num1: "",
+            num2: "",
+            operator: "",
+            displayOperator: "",
+            isActiveOperator: false,
+        }
+    }
+
     displayBtnData = (data) => {
         return data.map((dataElement) => {
             if(dataElement.type === "number") {
                 return <button className= "button" onClick={()=>this.handleNumberInput(dataElement)}>{dataElement.display}</button>
             }
-            if(dataElement.type === "operator") {
+            if(dataElement.type === "operation") {
                 return <button className= "button" onClick={()=>{this.handleOperatorInput(dataElement)}}>{dataElement.display}</button>
             }
             if(dataElement.type === "feature") {    
@@ -20,10 +32,70 @@ class Keypad extends Component{
         })
     }  
 
+    handleNumberInput = (numObj) => {
+        let {isActiveOperator, num1, num2} = this.state;
+    
+        if(!isActiveOperator) {  
+            this.setState({
+              num1: num1 + numObj.display,
+            })
+        } else {
+            this.setState({
+              num2: num2 + numObj.display
+            })
+        }
+      }
+
+    handleOperatorInput = (operatorObj) => {
+        this.setState({
+          operator: operatorObj.value,
+          displayOperator: operatorObj.display,
+          isActiveOperator: true,
+        })
+    }
+
+    handleFeatures = (feature) => {
+        switch(feature.value) {
+          case "clearAll": this.featureClearAll(); break;
+          case "clearCurr": this.featureClearCurrent(); break; 
+          case "posNeg": this.featurePosNeg(this.state.num1, this.state.num2); break;
+          case "percent": this.featurePercent(); break;
+          case "decimal": this.featureDecimal(); break;
+          default: return "testing";
+        }
+    }
+
+    handleCalculation = () => {
+        const {num1, num2, operator} = this.state;
+        let total = 0;
+        switch(operator) {
+          case "add": total = this.operationAdd(num1, num2); break;
+          case "subtract": total = this.operationSubtract(num1, num2); break;
+          case "multiply": total = this.operationMultiply(num1, num2); break;
+          case "divide": total = this.operationDivide(num1, num2); break;
+          default: return "testing";
+        }
+        this.setState({
+          result: total,
+          num1: total,
+          num2: "",
+          operator: "",
+          displayOperator: "",
+          isActiveOperator: false,
+        })
+      }
+
     render() {
+        let {result, num1, num2, displayOperator} = this.state;
         return(
             <div id="keypad">
-                <div id="display"><Display /></div>
+                <div id="display">
+                    <Display result = {result} 
+                             num1 = {num1}
+                             num2 = {num2}
+                             displayOperator = {displayOperator}
+                    />
+                </div>
                 <div id="display-buttons">{this.displayBtnData(buttons)}</div>
             </div>
         )
